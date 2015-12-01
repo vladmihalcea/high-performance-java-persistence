16c0ad1b970e08333fdd76f9163a0f66e506b947
vladmihalcea <mih_vlad@yahoo.com>
2015-12-01 13:54:13 +0200

    Add TupleTest

---
+++ core/src/test/java/com/vladmihalcea/book/hpjp/hibernate/criteria/TupleTest.java
@@ -0,0 +1,57 @@
+package com.vladmihalcea.book.hpjp.hibernate.criteria;
+
+import com.vladmihalcea.book.hpjp.util.AbstractTest;
+import com.vladmihalcea.book.hpjp.util.providers.BlogEntityProvider;
+import org.junit.Test;
+
+import javax.persistence.Tuple;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Path;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * EntityGraphMapperTest - Test mapping to entity
+ *
+ * @author Vlad Mihalcea
+ */
+public class TupleTest extends AbstractTest {
+
+    private BlogEntityProvider entityProvider = new BlogEntityProvider();
+
+    @Override
+    protected Class<?>[] entities() {
+        return entityProvider.entities();
+    }
+
+    @Override
+    public void init() {
+        super.init();
+        doInJPA(entityManager -> {
+            entityManager.persist(new BlogEntityProvider.Post(1L));
+            entityManager.persist(new BlogEntityProvider.Post(2L));
+        });
+    }
+
+    @Test
+    public void testTuple() {
+        doInJPA(entityManager -> {
+            CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+            CriteriaQuery<Tuple> cq = cb.createTupleQuery();
+
+            Root<BlogEntityProvider.Post> postRoot = cq.from(BlogEntityProvider.Post.class);
+            Path<Long> idPath = postRoot.get("id");
+            Path<String> titlePath = postRoot.get("title");
+            cq.multiselect(idPath, titlePath);
+
+            List<Tuple> resultList = entityManager.createQuery(cq).getResultList();
+
+            for (Tuple tuple : resultList) {
+                Long id = tuple.get(idPath);
+                String title = tuple.get(titlePath);
+            }
+        });
+    }
+}
