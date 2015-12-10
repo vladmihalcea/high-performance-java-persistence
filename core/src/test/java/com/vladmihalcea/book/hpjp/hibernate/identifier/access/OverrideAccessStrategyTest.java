329f8a695ce65722b040fc994c2c25e04eb08b9c
vladmihalcea <mih_vlad@yahoo.com>
2015-12-10 15:45:24 +0200

    Add examples for access strategy

---
+++ core/src/test/java/com/vladmihalcea/book/hpjp/hibernate/identifier/access/OverrideAccessStrategyTest.java
@@ -0,0 +1,63 @@
+package com.vladmihalcea.book.hpjp.hibernate.identifier.access;
+
+import com.vladmihalcea.book.hpjp.util.AbstractTest;
+import org.junit.Test;
+
+import javax.persistence.*;
+
+/**
+ * <code>OverrideAccessStrategyTest</code> - Override access strategy
+ *
+ * @author Vlad Mihalcea
+ */
+public class OverrideAccessStrategyTest extends AbstractTest {
+
+    @Override
+    protected Class<?>[] entities() {
+        return new Class<?>[] {
+            FieldEntity.class
+        };
+    }
+
+    @Test
+    public void testSequenceIdentifierGenerator() {
+        doInJPA(entityManager -> {
+            FieldEntity entity = new FieldEntity();
+            entity.setId(1);
+            entityManager.persist(entity);
+        });
+        doInJPA(entityManager -> {
+            FieldEntity entity = entityManager.find(FieldEntity.class, 1);
+            entity.setName("abc");
+        });
+    }
+
+    @Entity(name = "FieldEntity")
+    public static class FieldEntity {
+
+        private Integer id;
+
+        private String name;
+
+        @Version
+        @Access(AccessType.FIELD)
+        private Long version;
+
+        @Id
+        public Integer getId() {
+            return id;
+        }
+
+        public void setId(Integer id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
