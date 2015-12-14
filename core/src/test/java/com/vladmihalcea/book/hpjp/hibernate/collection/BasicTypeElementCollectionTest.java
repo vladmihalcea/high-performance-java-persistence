de45fd1a7881326f5829e7a566f31d77577e4601
vladmihalcea <mih_vlad@yahoo.com>
2015-12-14 21:30:59 +0200

    Add tests for collections

---
+++ core/src/test/java/com/vladmihalcea/book/hpjp/hibernate/collection/BasicTypeElementCollectionTest.java
@@ -0,0 +1,80 @@
+package com.vladmihalcea.book.hpjp.hibernate.collection;
+
+import com.vladmihalcea.book.hpjp.util.AbstractTest;
+import org.junit.Test;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * <code>BasicTypeElementCollectionTest</code> - Basic Type Element Collection Test
+ *
+ * @author Vlad Mihalcea
+ */
+public class BasicTypeElementCollectionTest extends AbstractTest {
+
+    @Override
+    protected Class<?>[] entities() {
+        return new Class<?>[] {
+            Person.class
+        };
+    }
+
+    @Override
+    public void init() {
+        super.init();
+        doInJPA(entityManager -> {
+            Person person = new Person();
+            person.id = 1L;
+            person.phones.add("027-123-4567");
+            person.phones.add("028-234-9876");
+            entityManager.persist(person);
+        });
+    }
+
+    @Test
+    public void testProxies() {
+        doInJPA(entityManager -> {
+            Person person = entityManager.find(Person.class, 1L);
+            assertEquals(2, person.getPhones().size());
+            try {
+                ArrayList<String> phones = (ArrayList<String>) person.getPhones();
+            } catch (Exception expected) {
+                LOGGER.error("Failure", expected);
+            }
+        });
+    }
+
+    @Test
+    public void testLifecycle() {
+        doInJPA(entityManager -> {
+            Person person = entityManager.find(Person.class, 1L);
+            LOGGER.info("Clear element collection and add element");
+            person.getPhones().clear();
+            person.getPhones().add("123-456-7890");
+            person.getPhones().add("456-000-1234");
+        });
+        doInJPA(entityManager -> {
+            Person person = entityManager.find(Person.class, 1L);
+            LOGGER.info("Remove one element");
+            person.getPhones().remove(0);
+        });
+    }
+
+    @Entity(name = "Person")
+    public static class Person  {
+
+        @Id
+        private Long id;
+
+        @ElementCollection
+        private List<String> phones = new ArrayList<>();
+
+        public List<String> getPhones() {
+            return phones;
+        }
+    }
+}
