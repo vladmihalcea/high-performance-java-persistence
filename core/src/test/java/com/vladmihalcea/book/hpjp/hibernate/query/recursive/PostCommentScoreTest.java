053b925210b5cf08e7291d5eefc2167090c7edab
Vlad Mihalcea <mih_vlad@yahoo.com>
2016-03-13 16:22:22 +0200

    Layout

--- core/src/test/java/com/vladmihalcea/book/hpjp/hibernate/query/recursive/PostCommentScoreTest.java
+++ core/src/test/java/com/vladmihalcea/book/hpjp/hibernate/query/recursive/PostCommentScoreTest.java
@@ -182,58 +182,48 @@ public class PostCommentScoreTest extends AbstractPostgreSQLIntegrationTest {
     public void test() {
         LOGGER.info("Recursive CTE and Window Functions");
         doInJPA(entityManager -> {
+            Long postId = 1L;
+            int rank = 3;
             List<PostCommentScore> result = entityManager.createNativeQuery(
                 "SELECT id, parent_id, root_id, review, created_on, score " +
                 "FROM ( " +
-                "    SELECT id, parent_id, root_id, review, created_on, score, dense_rank() OVER (ORDER BY total_score DESC) rank " +
+                "    SELECT " +
+                "        id, parent_id, root_id, review, created_on, score, " +
+                "        dense_rank() OVER (ORDER BY total_score DESC) rank " +
                 "    FROM ( " +
-                "        WITH RECURSIVE post_comment_score(id, root_id, post_id, parent_id, " +
-                "           review, created_on, score) AS ( " +
-                "           SELECT " +
-                "               id, id, post_id, parent_id, review, created_on, " +
-                "               SUM(" +
-                "                   CASE WHEN up IS NULL " +
-                "                   THEN 0 " +
-                "                   ELSE ( CASE WHEN up = true THEN 1 ELSE - 1 END ) " +
-                "                   END " +
-                "               ) score " +
+                "       WITH RECURSIVE post_comment_score(id, root_id, post_id, parent_id, " +
+                "           review, created_on, score) AS (" +
+                "           SELECT id, id, post_id, parent_id, review, created_on, " +
+                "               SUM( CASE WHEN up IS NULL THEN 0 ELSE ( " +
+                "               CASE WHEN up = true THEN 1 ELSE - 1 END ) END ) score " +
                 "           FROM post_comment " +
                 "           LEFT JOIN post_comment_vote ON comment_id = id " +
                 "           WHERE post_id = :postId AND parent_id IS NULL " +
                 "           GROUP BY id, id, post_id, parent_id, review, created_on " +
                 "           UNION ALL " +
-                "           SELECT " +
-                "               pc.id, pcs.root_id, pc.post_id, pc.parent_id, pc.review, " +
-                "               pc.created_on, ( " +
-                "               CASE WHEN pcv.up IS NULL " +
-                "               THEN 0 " +
-                "               ELSE ( CASE WHEN pcv.up = true THEN 1 ELSE - 1 END ) " +
-                "               END " +
-                "           ) score " +
-                "           FROM post_comment pc " +
-                "           LEFT JOIN post_comment_vote pcv ON pcv.comment_id = pc.id " +
-                "           INNER JOIN post_comment_score pcs ON pc.parent_id = pcs.id " +
-                "           WHERE pc.post_id = :postId " +
-                "        ) " +
-                "        SELECT " +
-                "            id, parent_id, root_id, review, created_on, score, " +
-                "            sum(score) OVER root_win total_score " +
-                "        FROM (" +
-                "           SELECT " +
-                "               id, parent_id, root_id, review, created_on, sum(score) score " +
+                "           SELECT pc.id, pcs.root_id, pc.post_id, pc.parent_id, pc.review, " +
+                "               pc.created_on, ( CASE WHEN pcv.up IS NULL THEN 0 ELSE ( " +
+                "               CASE WHEN pcv.up = true THEN 1 ELSE - 1 END ) END ) score " +
+                "          FROM post_comment pc " +
+                "          LEFT JOIN post_comment_vote pcv ON pcv.comment_id = pc.id " +
+                "          INNER JOIN post_comment_score pcs ON pc.parent_id = pcs.id " +
+                "          WHERE pc.post_id = :postId " +
+                "       ) " +
+                "       SELECT " +
+                "           id, parent_id, root_id, review, created_on, score, " +
+                "           SUM(score) OVER (PARTITION BY root_id) total_score " +
+                "       FROM (" +
+                "           SELECT id, parent_id, root_id, review, created_on, " +
+                "               SUM(score) score" +
                 "           FROM post_comment_score " +
                 "           GROUP BY id, parent_id, root_id, review, created_on " +
-                "        ) aggr " +
-                "        WINDOW root_win AS (PARTITION BY root_id ) " +
-                "    ) total_scores " +
+                "       ) score_by_comment " +
+                "    ) score_total " +
                 "    ORDER BY total_score DESC, created_on ASC " +
-                ") total " +
-                "WHERE rank <= :rank", "PostCommentScore")
-            .setParameter("postId", 1L)
-            .setParameter("rank", 3)
-            .unwrap(SQLQuery.class)
-            .setResultTransformer(new PostCommentScoreResultTransformer())
-            .list();
+                ") total_score_group " +
+                "WHERE rank <= :rank", "PostCommentScore").unwrap(SQLQuery.class)
+            .setParameter("postId", postId).setParameter("rank", rank)
+            .setResultTransformer(new PostCommentScoreResultTransformer()).list();
             assertEquals(3, result.size());
             result.get(0).getTotalScore();
         });
