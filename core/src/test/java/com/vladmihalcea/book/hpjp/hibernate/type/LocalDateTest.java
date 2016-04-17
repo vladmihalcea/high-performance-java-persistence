1c9531987e1fe5a2777e7effda276b8eeb9b926f
Vlad Mihalcea <mih_vlad@yahoo.com>
2016-04-17 16:18:39 +0300

    Add trunc/cast timestamp date tests

--- core/src/test/java/com/vladmihalcea/book/hpjp/hibernate/type/LocalDateTest.java
+++ core/src/test/java/com/vladmihalcea/book/hpjp/hibernate/type/LocalDateTest.java
@@ -1,15 +1,15 @@
 package com.vladmihalcea.book.hpjp.hibernate.type;
 
+import com.vladmihalcea.book.hpjp.util.AbstractPostgreSQLIntegrationTest;
 import com.vladmihalcea.book.hpjp.util.AbstractTest;
 import org.junit.Test;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
-import java.time.LocalDate;
-import java.time.OffsetDateTime;
-import java.time.ZoneOffset;
+import java.sql.Timestamp;
+import java.time.*;
+import java.util.Date;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -24,7 +24,8 @@ public class LocalDateTest extends AbstractTest {
     protected Class<?>[] entities() {
         return new Class<?>[] {
             LocalDateEvent.class,
-            OffsetDateTimeEvent.class
+            OffsetDateTimeEvent.class,
+            TimestampEvent.class
         };
     }
 
@@ -66,6 +67,45 @@ public class LocalDateTest extends AbstractTest {
         });
     }
 
+    @Test
+    public void testTruncEvent() {
+        doInJPA(entityManager -> {
+            TimestampEvent event = new TimestampEvent();
+            event.id = 1L;
+            event.createdOn = new Date();
+            entityManager.persist(event);
+        });
+
+        doInJPA(entityManager -> {
+            List<TimestampEvent> events = entityManager
+                .createQuery(
+                    "select e " +
+                    "from TimestampEvent e " +
+                    "where cast(e.createdOn as date) >= :createdOn " +
+                    "order by e.createdOn asc")
+                .setParameter("createdOn", Timestamp.from(LocalDateTime.of(LocalDate.now(), LocalTime.MIDNIGHT).toInstant(ZoneOffset.UTC)), TemporalType.DATE)
+                //.setParameter("createdOn", new Date(0), TemporalType.DATE)
+                .getResultList();
+            assertEquals(1, events.size());
+        });
+        doInJPA(entityManager -> {
+
+            LocalDateTime dt = LocalDateTime.now();
+            ZonedDateTime zdt = dt.atZone(ZoneOffset.systemDefault());
+            ZoneOffset offset = zdt.getOffset();
+
+            List<TimestampEvent> events = entityManager
+                .createQuery(
+                    "select e " +
+                    "from TimestampEvent e " +
+                    "where function('trunc', e.createdOn) >= :createdOn " +
+                    "order by e.createdOn asc")
+                .setParameter("createdOn", Timestamp.from(LocalDateTime.of(LocalDate.now(), LocalTime.MIDNIGHT).minusSeconds(offset.getTotalSeconds()).toInstant(ZoneOffset.UTC)), TemporalType.DATE)
+                .getResultList();
+            assertEquals(1, events.size());
+        });
+    }
+
     @Entity(name = "LocalDateEvent")
     public static class LocalDateEvent {
 
@@ -86,5 +126,16 @@ public class LocalDateTest extends AbstractTest {
         @NotNull
         @Column(name = "START_DATE", nullable = false)
         private OffsetDateTime startDate;
+    }
+
+    @Entity(name = "TimestampEvent")
+    public static class TimestampEvent {
+
+        @Id
+        private Long id;
+
+        @Column(name = "START_DATE")
+        @Temporal(TemporalType.TIMESTAMP)
+        private Date createdOn;
     }
 }
