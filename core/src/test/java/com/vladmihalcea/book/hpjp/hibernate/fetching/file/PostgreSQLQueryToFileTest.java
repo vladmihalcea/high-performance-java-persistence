42582d6f54817aea79de5eeeb90686123ca37262
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2016-11-29 12:41:08 +0200

    Add GeneratedTest

---
+++ core/src/test/java/com/vladmihalcea/book/hpjp/hibernate/fetching/file/PostgreSQLQueryToFileTest.java
@@ -0,0 +1,156 @@
+package com.vladmihalcea.book.hpjp.hibernate.fetching.file;
+
+import com.vladmihalcea.book.hpjp.util.AbstractPostgreSQLIntegrationTest;
+import org.junit.Test;
+
+import javax.persistence.*;
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * @author Vlad Mihalcea
+ */
+public class PostgreSQLQueryToFileTest extends AbstractPostgreSQLIntegrationTest {
+
+    @Override
+    protected Properties properties() {
+        Properties properties = super.properties();
+        //properties.setProperty(AvailableSettings.USE_STREAMS_FOR_BINARY, Boolean.FALSE.toString());
+        return properties;
+    }
+
+    @Override
+    protected Class<?>[] entities() {
+        return new Class<?>[]{
+                Post.class,
+                PostComment.class,
+        };
+    }
+
+
+    @Override
+    public void init() {
+        super.init();
+        doInJPA(entityManager -> {
+            for (long id = 1; id <= 100; id++) {
+                Post post = new Post();
+                post.setId(id);
+                post.setTitle("High-Performance Java Persistence");
+
+                post.addComment(new PostComment("Excellent!"));
+                post.addComment(new PostComment("Great!"));
+
+                entityManager.persist(post);
+            }
+        });
+    }
+
+    @Test
+    public void testCopy() throws URISyntaxException {
+        doInJPA(entityManager -> {
+            entityManager.createNativeQuery(
+                String.format(
+                    "copy (" +
+                    "   select * " +
+                    "   from post p " +
+                    "   inner join post_comment pc on pc.post_id = p.id " +
+                    ") " +
+                    "to '%s' " +
+                    "with CSV DELIMITER ','",
+                    Paths.get(System.getenv("PGSQL_DATA"), "post_and_comments.csv").toString()
+                )
+            )
+            .executeUpdate();
+        });
+    }
+
+    @Entity(name = "Post")
+    @Table(name = "post")
+    public static class Post {
+
+        @Id
+        private Long id;
+
+        private String title;
+
+        @OneToMany(cascade = CascadeType.ALL, mappedBy = "post",
+                orphanRemoval = true)
+        private List<PostComment> comments = new ArrayList<>();
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public void addComment(PostComment comment) {
+            comments.add(comment);
+            comment.setPost(this);
+        }
+
+        @Override
+        public String toString() {
+            return "Post{" +
+                    "id=" + id +
+                    ", title='" + title + '\'' +
+                    '}';
+        }
+    }
+
+    @Entity(name = "PostComment")
+    @Table(name = "post_comment")
+    public static class PostComment {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        private Long id;
+
+        @ManyToOne(fetch = FetchType.LAZY)
+        private Post post;
+
+        private String review;
+
+        public PostComment() {}
+
+        public PostComment(String review) {
+            this.review = review;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public Post getPost() {
+            return post;
+        }
+
+        public void setPost(Post post) {
+            this.post = post;
+        }
+
+        public String getReview() {
+            return review;
+        }
+
+        public void setReview(String review) {
+            this.review = review;
+        }
+    }
+}
---
+++ core/src/test/java/com/vladmihalcea/book/hpjp/hibernate/mapping/GeneratedTest.java
@@ -0,0 +1,162 @@
+package com.vladmihalcea.book.hpjp.hibernate.mapping;
+
+import com.vladmihalcea.book.hpjp.util.AbstractSQLServerIntegrationTest;
+import org.hibernate.annotations.Generated;
+import org.hibernate.annotations.GenerationTime;
+import org.junit.Test;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Vlad Mihalcea
+ */
+public class GeneratedTest extends AbstractSQLServerIntegrationTest {
+
+	@Override
+	protected Class<?>[] entities() {
+		return new Class<?>[] {
+			Hero.class
+		};
+	}
+
+	@Test
+	public void test() {
+		doInJPA( entityManager -> {
+			Hero heroine = new Hero();
+			heroine.setId( 1L );
+
+			heroine.setFirstName( "Agustina" );
+			heroine.setMiddleName1( "Raimunda" );
+			heroine.setMiddleName2( "María" );
+			heroine.setMiddleName3( "Saragossa" );
+			heroine.setLastName( "Domènech" );
+
+			entityManager.persist( heroine );
+			LOGGER.info("After entity persist action");
+			entityManager.flush();
+
+			assertEquals("Agustina Raimunda María Saragossa Domènech", heroine.getFullName());
+		} );
+		doInJPA( entityManager -> {
+			Hero heroine = entityManager.find( Hero.class, 1L );
+			heroine.setMiddleName1( null );
+			heroine.setMiddleName2( null );
+			heroine.setMiddleName3( null );
+			heroine.setLastName( "de Aragón" );
+
+			LOGGER.info("After entity update action");
+			entityManager.flush();
+
+			assertEquals("Agustina de Aragón", heroine.getFullName());
+		} );
+	}
+
+	@Entity(name = "Hero")
+	public static class Hero {
+
+		@Id
+		private Long id;
+
+		private String firstName;
+
+		private String lastName;
+
+		private String middleName1;
+
+		private String middleName2;
+
+		private String middleName3;
+
+		private String middleName4;
+
+		private String middleName5;
+
+		@Generated( value = GenerationTime.ALWAYS )
+		@Column(columnDefinition =
+			"AS CONCAT(" +
+			"	COALESCE(firstName, ''), " +
+			"	COALESCE(' ' + middleName1, ''), " +
+			"	COALESCE(' ' + middleName2, ''), " +
+			"	COALESCE(' ' + middleName3, ''), " +
+			"	COALESCE(' ' + middleName4, ''), " +
+			"	COALESCE(' ' + middleName5, ''), " +
+			"	COALESCE(' ' + lastName, '') " +
+			")")
+		private String fullName;
+
+		//Getters and setters omitted for brevity
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getFirstName() {
+			return firstName;
+		}
+
+		public void setFirstName(String firstName) {
+			this.firstName = firstName;
+		}
+
+		public String getLastName() {
+			return lastName;
+		}
+
+		public void setLastName(String lastName) {
+			this.lastName = lastName;
+		}
+
+		public String getMiddleName1() {
+			return middleName1;
+		}
+
+		public void setMiddleName1(String middleName1) {
+			this.middleName1 = middleName1;
+		}
+
+		public String getMiddleName2() {
+			return middleName2;
+		}
+
+		public void setMiddleName2(String middleName2) {
+			this.middleName2 = middleName2;
+		}
+
+		public String getMiddleName3() {
+			return middleName3;
+		}
+
+		public void setMiddleName3(String middleName3) {
+			this.middleName3 = middleName3;
+		}
+
+		public String getMiddleName4() {
+			return middleName4;
+		}
+
+		public void setMiddleName4(String middleName4) {
+			this.middleName4 = middleName4;
+		}
+
+		public String getMiddleName5() {
+			return middleName5;
+		}
+
+		public void setMiddleName5(String middleName5) {
+			this.middleName5 = middleName5;
+		}
+
+		public String getFullName() {
+			return fullName;
+		}
+
+	}
+}
