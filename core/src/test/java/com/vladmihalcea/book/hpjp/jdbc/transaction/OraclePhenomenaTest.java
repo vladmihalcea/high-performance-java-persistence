93137d317907baafa2fd7c366869ec1d5b4d50ed
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2017-01-28 18:42:30 +0200

    Limit Oracle two just 2 isolation levels

--- core/src/test/java/com/vladmihalcea/book/hpjp/jdbc/transaction/OraclePhenomenaTest.java
+++ core/src/test/java/com/vladmihalcea/book/hpjp/jdbc/transaction/OraclePhenomenaTest.java
@@ -1,5 +1,12 @@
 package com.vladmihalcea.book.hpjp.jdbc.transaction;
 
+import org.junit.runners.Parameterized;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 /**
  * OraclePhenomenaTest - Test to validate Oracle phenomena
  *
@@ -9,6 +16,14 @@ public class OraclePhenomenaTest extends AbstractPhenomenaTest {
 
     public OraclePhenomenaTest(String isolationLevelName, int isolationLevel) {
         super(isolationLevelName, isolationLevel);
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> isolationLevels() {
+        List<Object[]> levels = new ArrayList<>();
+        levels.add(new Object[]{"Read Committed", Connection.TRANSACTION_READ_COMMITTED});
+        levels.add(new Object[]{"Serializable", Connection.TRANSACTION_SERIALIZABLE});
+        return levels;
     }
 
     @Override
