9c63863922d7b5a543e40465fcdca42e5458cb60
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2017-04-20 17:10:03 +0300

    Improve batch processing example

--- core/src/test/java/com/vladmihalcea/book/hpjp/hibernate/flushing/BatchProcessingTest.java
+++ core/src/test/java/com/vladmihalcea/book/hpjp/hibernate/flushing/BatchProcessingTest.java
@@ -1,5 +1,7 @@
 package com.vladmihalcea.book.hpjp.hibernate.flushing;
 
+import java.util.Properties;
+
 import com.vladmihalcea.book.hpjp.util.AbstractTest;
 import org.junit.Test;
 
@@ -17,13 +19,24 @@ public class BatchProcessingTest extends AbstractTest {
         };
     }
 
+    @Override
+    protected Properties properties() {
+        Properties properties = super.properties();
+        properties.put("hibernate.jdbc.batch_size", "25");
+        properties.put("hibernate.order_inserts", "true");
+        properties.put("hibernate.order_updates", "true");
+        properties.put("hibernate.jdbc.batch_versioned_data", "true");
+        return properties;
+    }
+
     @Test
     public void testBatchProcessing() {
-        int entityCount = 100;
+        int entityCount = 50;
         int batchSize = 25;
 
         EntityManager entityManager = null;
         EntityTransaction transaction = null;
+
         try {
             entityManager = entityManagerFactory().createEntityManager();
 
@@ -31,15 +44,16 @@ public class BatchProcessingTest extends AbstractTest {
             transaction.begin();
 
             for ( int i = 0; i < entityCount; ++i ) {
-                Post post = new Post( String.format( "Post %d", i ) );
-                entityManager.persist( post );
-                if ( i % batchSize == 0 ) {
+                if ( i > 0 && i % batchSize == 0 ) {
                     entityManager.flush();
                     entityManager.clear();
 
                     transaction.commit();
                     transaction.begin();
                 }
+
+                Post post = new Post( String.format( "Post %d", i + 1 ) );
+                entityManager.persist( post );
             }
 
             transaction.commit();
