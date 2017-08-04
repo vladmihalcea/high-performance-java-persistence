b0b114d0d3500556f35041f12c418288bef81567
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2017-08-04 13:18:07 +0300

    Update JSON test for update as well

--- core/src/test/java/com/vladmihalcea/book/hpjp/hibernate/type/json/PostgreSQLJsonNodeBinaryTypeTest.java
+++ core/src/test/java/com/vladmihalcea/book/hpjp/hibernate/type/json/PostgreSQLJsonNodeBinaryTypeTest.java
@@ -33,28 +33,40 @@ public class PostgreSQLJsonNodeBinaryTypeTest extends AbstractPostgreSQLIntegrat
 
         doInJPA(entityManager -> {
 
-            Book configuration = new Book();
-            configuration.setIsbn( "978-9730228236" );
-            configuration.setProperties(
+            Book book = new Book();
+            book.setIsbn( "978-9730228236" );
+            book.setProperties(
                 JacksonUtil.toJsonNode(
                     "{" +
                     "   \"title\": \"High-Performance Java Persistence\"," +
                     "   \"author\": \"Vlad Mihalcea\"," +
-                    "   \"publisher\": \"Amazon\", " +
+                    "   \"publisher\": \"Amazon\"," +
                     "   \"price\": 44.99" +
                     "}"
                 )
             );
 
-            entityManager.persist( configuration );
+            entityManager.persist( book );
         });
         doInJPA(entityManager -> {
             Session session = entityManager.unwrap( Session.class );
-            Book configuration = session
+            Book book = session
                 .bySimpleNaturalId( Book.class )
                 .load( "978-9730228236" );
 
-            LOGGER.info( "Book details; {}", configuration.getProperties() );
+            LOGGER.info( "Book details: {}", book.getProperties() );
+
+            book.setProperties(
+                JacksonUtil.toJsonNode(
+                    "{" +
+                    "   \"title\": \"High-Performance Java Persistence\"," +
+                    "   \"author\": \"Vlad Mihalcea\"," +
+                    "   \"publisher\": \"Amazon\"," +
+                    "   \"price\": 44.99," +
+                    "   \"url\": \"https://www.amazon.com/High-Performance-Java-Persistence-Vlad-Mihalcea/dp/973022823X/\"" +
+                    "}"
+                )
+            );
         });
     }
 
