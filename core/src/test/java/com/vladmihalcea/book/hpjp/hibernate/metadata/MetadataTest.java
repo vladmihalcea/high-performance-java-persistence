79e556410dd7923b7cee3a7a40647a5e6b6931ec
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2017-08-24 09:48:38 +0300

    Add a more cocmpelling example to binding metadata

--- core/src/test/java/com/vladmihalcea/book/hpjp/hibernate/metadata/MetadataTest.java
+++ core/src/test/java/com/vladmihalcea/book/hpjp/hibernate/metadata/MetadataTest.java
@@ -7,62 +7,22 @@ import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 import org.hibernate.boot.Metadata;
-import org.hibernate.boot.model.relational.Database;
 import org.hibernate.boot.model.relational.Namespace;
-import org.hibernate.cfg.annotations.EntityBinder;
-import org.hibernate.engine.spi.SessionFactoryImplementor;
 import org.hibernate.integrator.spi.Integrator;
 import org.hibernate.mapping.Column;
 import org.hibernate.mapping.PersistentClass;
 import org.hibernate.mapping.Property;
 import org.hibernate.mapping.Table;
-import org.hibernate.service.spi.SessionFactoryServiceRegistry;
 
 import org.junit.Test;
 
 import com.vladmihalcea.book.hpjp.util.AbstractMySQLIntegrationTest;
-import com.vladmihalcea.book.hpjp.util.AbstractTest;
 import com.vladmihalcea.book.hpjp.util.providers.entity.BlogEntityProvider;
 
 /**
  * @author Vlad Mihalcea
  */
 public class MetadataTest extends AbstractMySQLIntegrationTest {
-
-    public static class MetadataExtractorIntegrator implements org.hibernate.integrator.spi.Integrator {
-
-        public static final MetadataExtractorIntegrator INSTANCE = new MetadataExtractorIntegrator();
-
-        private Database database;
-
-        private Metadata metadata;
-
-        public Database getDatabase() {
-            return database;
-        }
-
-        public Metadata getMetadata() {
-            return metadata;
-        }
-
-        @Override
-        public void integrate(
-                Metadata metadata,
-                SessionFactoryImplementor sessionFactory,
-                SessionFactoryServiceRegistry serviceRegistry) {
-
-            this.database = metadata.getDatabase();
-            this.metadata = metadata;
-
-        }
-
-        @Override
-        public void disintegrate(
-            SessionFactoryImplementor sessionFactory,
-            SessionFactoryServiceRegistry serviceRegistry) {
-
-        }
-    }
 
     @Override
     protected Integrator integrator() {
@@ -75,7 +35,7 @@ public class MetadataTest extends AbstractMySQLIntegrationTest {
     }
 
     @Test
-    public void test() {
+    public void testDatabaseMetadata() {
         for(Namespace namespace : MetadataExtractorIntegrator.INSTANCE.getDatabase().getNamespaces()) {
             for( Table table : namespace.getTables()) {
                 LOGGER.info( "Table {} has the following columns: {}",
@@ -88,23 +48,28 @@ public class MetadataTest extends AbstractMySQLIntegrationTest {
     }
 
     @Test
-    public void testMetadata() {
+    public void testEntityToDatabaseBindingMetadata() {
         Metadata metadata = MetadataExtractorIntegrator.INSTANCE.getMetadata();
 
-        PersistentClass persistentClass = metadata.getEntityBinding( BlogEntityProvider.Post.class.getName());
-        Table table = persistentClass.getTable();
+        for ( PersistentClass persistentClass : metadata.getEntityBindings()) {
+            Table table = persistentClass.getTable();
+            LOGGER.info( "Entity: {} is mapped to table: {}",
+                         persistentClass.getClassName(),
+                         table.getName()
+            );
 
-        for(Iterator propertyIterator =
-                persistentClass.getPropertyIterator(); propertyIterator.hasNext(); ) {
-           Property property = (Property) propertyIterator.next();
-            for(Iterator columnIterator =
-                    property.getColumnIterator(); columnIterator.hasNext(); ) {
-                Column column = (Column) columnIterator.next();
-                LOGGER.info( "Table {}, column {} associated to property: {}",
-                         table.getName(),
-                         column.getName(),
-                         property.getName()
-                );
+            for(Iterator propertyIterator =
+                    persistentClass.getPropertyIterator(); propertyIterator.hasNext(); ) {
+                Property property = (Property) propertyIterator.next();
+                for(Iterator columnIterator =
+                        property.getColumnIterator(); columnIterator.hasNext(); ) {
+                    Column column = (Column) columnIterator.next();
+                    LOGGER.info( "Property: {} is mapped on table column: {} of type: {}",
+                                 property.getName(),
+                                 column.getName(),
+                                 column.getSqlType()
+                    );
+                }
             }
         }
     }
