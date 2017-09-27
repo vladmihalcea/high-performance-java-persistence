066d5bc6a70c35b2e64c38fab7ebce56dfc352d4
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2017-09-27 20:44:05 +0300

    Elaborate statistics test to better prove how it works

--- core/src/test/java/com/vladmihalcea/book/hpjp/hibernate/statistics/ConnectionStatisticsTest.java
+++ core/src/test/java/com/vladmihalcea/book/hpjp/hibernate/statistics/ConnectionStatisticsTest.java
@@ -3,11 +3,19 @@ package com.vladmihalcea.book.hpjp.hibernate.statistics;
 import com.vladmihalcea.book.hpjp.util.AbstractTest;
 import com.vladmihalcea.book.hpjp.util.providers.entity.BlogEntityProvider;
 import org.hibernate.cfg.AvailableSettings;
+import org.hibernate.engine.jdbc.connections.internal.DatasourceConnectionProviderImpl;
+import org.hibernate.resource.jdbc.spi.PhysicalConnectionHandlingMode;
+import org.hibernate.stat.internal.StatisticsInitiator;
 import org.junit.Test;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Properties;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
 
-import static com.vladmihalcea.book.hpjp.util.providers.entity.BlogEntityProvider.*;
+import static com.vladmihalcea.book.hpjp.util.providers.entity.BlogEntityProvider.Post;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -20,36 +28,97 @@ public class ConnectionStatisticsTest extends AbstractTest {
         return BlogEntityProvider.INSTANCE.entities();
     }
 
-    @Test
-    public void testJdbcOneToManyMapping() {
-        int iterations = 5;
+    private final AtomicBoolean autoCommit = new AtomicBoolean(true);
 
-        for (long i = 0; i < iterations; i++) {
-            final Long id = i + 1;
-            doInJPA(entityManager -> {
-                Post post = new Post();
-                post.setId(id);
-                post.setTitle(
-                    String.format(
-                        "High-Performance Java Persistence, Part %d", id
-                    )
-                );
-                entityManager.persist(post);
+    private class SingletonDatasourceConnectionProvider
+            extends DatasourceConnectionProviderImpl {
 
-                Number postCount = entityManager.createQuery(
-                    "select count(*) from Post", Number.class)
-                .getSingleResult();
+        public SingletonDatasourceConnectionProvider(final DataSource dataSource) {
+            setDataSource(dataSource);
+        }
 
-                assertEquals((long) id, postCount.longValue());
-            });
+        final AtomicReference<Connection> connectionHolder = new AtomicReference<>();
+
+        @Override
+        public Connection getConnection() throws SQLException {
+            Connection connection = connectionHolder.get();
+            if(connection == null) {
+                connection = super.getConnection();
+                if (!autoCommit.get()) {
+                    connection.setAutoCommit(false);
+                }
+                connectionHolder.set(connection);
+            }
+            return connection;
+        }
+
+        @Override
+        public void closeConnection(Connection connection) throws SQLException {
+            if (autoCommit.get()) {
+                super.closeConnection(connection);
+                connectionHolder.set(null);
+            }
         }
     }
 
-    @Override
-    protected Properties properties() {
-        Properties properties = super.properties();
-        properties.put(AvailableSettings.GENERATE_STATISTICS, "true");
-        properties.put("hibernate.stats.factory", TransactionStatisticsFactory.class.getName());
-        return properties;
+    protected void additionalProperties(Properties properties) {
+        DataSource actualDataSource = (DataSource) properties.get(AvailableSettings.CONNECTION_PROVIDER);
+
+        properties.put(
+            AvailableSettings.CONNECTION_PROVIDER,
+            new SingletonDatasourceConnectionProvider(actualDataSource)
+        );
+
+        properties.put(
+            AvailableSettings.CONNECTION_HANDLING,
+            PhysicalConnectionHandlingMode.DELAYED_ACQUISITION_AND_RELEASE_AFTER_STATEMENT
+        );
+
+        properties.put(
+            AvailableSettings.CONNECTION_PROVIDER_DISABLES_AUTOCOMMIT,
+            Boolean.TRUE.toString()
+        );
+
+        properties.put(
+            AvailableSettings.GENERATE_STATISTICS,
+            Boolean.TRUE.toString()
+        );
+
+        properties.put(
+            StatisticsInitiator.STATS_BUILDER,
+            TransactionStatisticsFactory.class.getName()
+        );
+    }
+
+    @Test
+    public void test() {
+        int iterations = 5;
+
+        for (long i = 0; i < iterations; i++) {
+            autoCommit.set(false);
+            final Long id = i + 1;
+
+            doInJPA(entityManager -> {
+                try {
+                    Post post = new Post();
+                    post.setId(id);
+                    post.setTitle(
+                        String.format(
+                            "High-Performance Java Persistence, Part %d", id
+                        )
+                    );
+                    entityManager.persist(post);
+
+                    Number postCount = entityManager.createQuery(
+                        "select count(*) from Post", Number.class)
+                    .getSingleResult();
+
+                    assertEquals((long) id, postCount.longValue());
+                }
+                finally {
+                    autoCommit.set(true);
+                }
+            });
+        }
     }
 }
