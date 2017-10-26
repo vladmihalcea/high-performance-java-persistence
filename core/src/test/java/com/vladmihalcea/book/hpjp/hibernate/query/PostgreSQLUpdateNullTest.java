64e56c28831594649e30a674db071feebd4c3b61
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2017-10-26 08:14:03 +0300

    Add PostgreSQL update null test

---
+++ core/src/test/java/com/vladmihalcea/book/hpjp/hibernate/query/PostgreSQLUpdateNullTest.java
@@ -0,0 +1,61 @@
+package com.vladmihalcea.book.hpjp.hibernate.query;
+
+import com.vladmihalcea.book.hpjp.util.AbstractPostgreSQLIntegrationTest;
+import com.vladmihalcea.book.hpjp.util.AbstractTest;
+import org.hibernate.Session;
+import org.junit.Test;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+/**
+ * @author Vlad Mihalcea
+ */
+public class PostgreSQLUpdateNullTest extends AbstractPostgreSQLIntegrationTest {
+
+    @Override
+    protected Class<?>[] entities() {
+        return new Class<?>[] {
+            Post.class
+        };
+    }
+
+    @Test
+    public void test() {
+        doInJPA(entityManager -> {
+            Post post = new Post();
+            post.id = 1L;
+            post.externalId = 123L;
+
+            entityManager.persist(post);
+        });
+
+        doInJPA(entityManager -> {
+            int count = entityManager.createQuery(
+                "update Post p " +
+                "set p.externalId = :externalId " +
+                "where p.id = :id")
+                .setParameter("externalId", null)
+                .setParameter("id", 1L)
+                .executeUpdate();
+            assertEquals(1, count);
+
+            Post post = entityManager.find(Post.class, 1L);
+            assertNull(post.externalId);
+        });
+    }
+
+    @Entity(name = "Post")
+    @Table(name = "post")
+    public static class Post {
+
+        @Id
+        private Long id;
+
+        private Long externalId;
+    }
+}
