ebf9a46dde88b35fcb8950903f38e4f12e33a44b
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2017-11-18 17:05:37 +0200

    Add @OneToOne N+1 query issue test

---
+++ core/src/test/java/com/vladmihalcea/book/hpjp/hibernate/association/BidirectionalOneToOneNPlusOneTest.java
@@ -0,0 +1,138 @@
+package com.vladmihalcea.book.hpjp.hibernate.association;
+
+import com.vladmihalcea.book.hpjp.util.AbstractTest;
+import org.junit.Test;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * @author Vlad Mihalcea
+ */
+public class BidirectionalOneToOneNPlusOneTest extends AbstractTest {
+
+    @Override
+    protected Class<?>[] entities() {
+        return new Class<?>[] {
+            Post.class,
+            PostDetails.class,
+        };
+    }
+
+    @Test
+    public void test() {
+        doInJPA(entityManager -> {
+            for (int i = 0; i < 3; i++) {
+                Post post = new Post(String.format("Post nr. %d", i));
+                post.setDetails(new PostDetails("Excellent!"));
+
+                entityManager.persist(post);
+            }
+        });
+
+        doInJPA(entityManager -> {
+            List<Post> posts = entityManager.createQuery(
+                "select p from Post p " +
+                "where p.title like 'Post nr.%'", Post.class)
+            .getResultList();
+        });
+    }
+
+    @Entity(name = "Post")
+    @Table(name = "post")
+    public static class Post {
+
+        @Id
+        @GeneratedValue
+        private Long id;
+
+        private String title;
+
+        @OneToOne(mappedBy = "post", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+        private PostDetails details;
+
+        public Post() {}
+
+        public Post(String title) {
+            this.title = title;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public PostDetails getDetails() {
+            return details;
+        }
+
+        public void setDetails(PostDetails details) {
+            this.details = details;
+            details.setPost(this);
+        }
+    }
+
+    @Entity(name = "PostDetails")
+    @Table(name = "post_details")
+    public static class PostDetails {
+
+        @Id
+        @GeneratedValue
+        private Long id;
+
+        @Column(name = "created_on")
+        private Date createdOn;
+
+        @Column(name = "created_by")
+        private String createdBy;
+
+        @OneToOne(fetch = FetchType.LAZY)
+        @MapsId
+        private Post post;
+
+        public PostDetails() {}
+
+        public PostDetails(String createdBy) {
+            createdOn = new Date();
+            this.createdBy = createdBy;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public Date getCreatedOn() {
+            return createdOn;
+        }
+
+        public String getCreatedBy() {
+            return createdBy;
+        }
+
+        public Post getPost() {
+            return post;
+        }
+
+        public void setPost(Post post) {
+            this.post = post;
+        }
+    }
+}
