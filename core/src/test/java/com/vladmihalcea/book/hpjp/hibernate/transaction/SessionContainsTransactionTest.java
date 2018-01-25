fc1de57ab71d3e7a7eb849360a90ddb45fcb8fb8
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2018-01-25 14:33:12 +0200

    Add test to prove that Session span is not influenced by transaction management

---
+++ core/src/test/java/com/vladmihalcea/book/hpjp/hibernate/transaction/SessionContainsTransactionTest.java
@@ -0,0 +1,85 @@
+package com.vladmihalcea.book.hpjp.hibernate.transaction;
+
+import com.vladmihalcea.book.hpjp.util.AbstractTest;
+import org.hibernate.Session;
+import org.jboss.logging.Logger;
+import org.junit.Test;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Id;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author Vlad Mihalcea
+ */
+public class SessionContainsTransactionTest extends AbstractTest {
+    
+    @Override
+    protected Class<?>[] entities() {
+        return new Class<?>[]{
+                Post.class
+        };
+    }
+
+    @Test
+    public void testSessionContains() {
+        EntityManager entityManager = null;
+        EntityTransaction txn = null;
+        try {
+            entityManager = entityManagerFactory().createEntityManager();
+
+            txn = entityManager.getTransaction();
+            txn.begin();
+
+            Post person = new Post(1L, "High-Performance Java Persistence");
+            entityManager.persist(person);
+
+            txn.commit();
+
+            txn = entityManager.getTransaction();
+            txn.begin();
+
+            assertTrue(entityManager.contains(person));
+
+            txn.commit();
+        } catch (RuntimeException e) {
+            if (txn != null && txn.isActive()) {
+                txn.rollback();
+            }
+            throw e;
+        } finally {
+            if (entityManager != null) {
+                entityManager.close();
+            }
+        }
+    }
+
+    @Entity(name = "Post")
+    public static class Post {
+
+        @Id
+        private Long id;
+
+        private String name;
+
+        public Post() {
+        }
+
+        public Post(long id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+}
