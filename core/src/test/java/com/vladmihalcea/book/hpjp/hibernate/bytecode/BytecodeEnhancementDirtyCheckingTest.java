f95d621b390e46ab0e2f17ffae8181f7904ecac6
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2018-02-17 12:25:13 +0200

    Add new test

---
+++ core/src/test/java/com/vladmihalcea/book/hpjp/hibernate/bytecode/BytecodeEnhancementDirtyCheckingTest.java
@@ -0,0 +1,37 @@
+package com.vladmihalcea.book.hpjp.hibernate.bytecode;
+
+import com.vladmihalcea.book.hpjp.hibernate.forum.Post;
+import com.vladmihalcea.book.hpjp.hibernate.forum.Tag;
+import com.vladmihalcea.book.hpjp.util.AbstractTest;
+import org.junit.Test;
+
+/**
+ * @author Vlad Mihalcea
+ */
+public class BytecodeEnhancementDirtyCheckingTest extends AbstractTest {
+
+    @Override
+    protected Class<?>[] entities() {
+        return new Class<?>[] {
+            Tag.class
+        };
+    }
+
+    @Test
+    public void testDirtyChecking() {
+        doInJPA(entityManager -> {
+            Tag tag = new Tag();
+            tag.setId(1L);
+            tag.setName("High-Performance Hibernate");
+
+            entityManager.persist(tag);
+        });
+
+        doInJPA(entityManager -> {
+            Tag tag = entityManager.find(Tag.class, 1L);
+
+            tag.setName("High-Performance Java Persistence");
+            entityManager.flush();
+        });
+    }
+}
