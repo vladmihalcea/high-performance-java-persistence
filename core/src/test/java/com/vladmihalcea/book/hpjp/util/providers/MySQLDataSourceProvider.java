0d348916b15db4026d47bcfaa3a7d4a55a5934da
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2018-03-11 17:34:55 +0200

    Fix old import issue

--- core/src/test/java/com/vladmihalcea/book/hpjp/util/providers/MySQLDataSourceProvider.java
+++ core/src/test/java/com/vladmihalcea/book/hpjp/util/providers/MySQLDataSourceProvider.java
@@ -1,6 +1,6 @@
 package com.vladmihalcea.book.hpjp.util.providers;
 
-import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import com.mysql.cj.jdbc.MysqlDataSource;
 
 import javax.sql.DataSource;
 import java.util.Properties;
