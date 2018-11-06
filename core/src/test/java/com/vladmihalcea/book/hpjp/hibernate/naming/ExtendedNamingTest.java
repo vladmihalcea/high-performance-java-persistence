c81d30a39162a6c5280f0fd27bdca934cd6537df
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2018-11-06 22:17:02 +0200

    Add CamelCase to SnakeCase example

--- core/src/test/java/com/vladmihalcea/book/hpjp/hibernate/naming/ExtendedNamingTest.java
+++ core/src/test/java/com/vladmihalcea/book/hpjp/hibernate/naming/ExtendedNamingTest.java
@@ -15,9 +15,10 @@ public class ExtendedNamingTest extends DefaultNamingTest {
     }
 
     @Override
-    protected Properties properties() {
-        Properties properties = super.properties();
-        properties.put("hibernate.physical_naming_strategy", "com.vladmihalcea.book.hpjp.hibernate.naming.OracleNamingStrategy");
-        return properties;
+    protected void additionalProperties(Properties properties) {
+        properties.put(
+                "hibernate.physical_naming_strategy",
+                OracleNamingStrategy.class.getName()
+        );
     }
 }
