056bae041138f5a47ba07183670dcdbb140d0ddc
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2019-01-18 10:41:35 +0200

    Test JMX functionality

--- core/src/test/java/com/vladmihalcea/book/hpjp/hibernate/jmx/JmxTest.java
+++ core/src/test/java/com/vladmihalcea/book/hpjp/hibernate/jmx/JmxTest.java
@@ -2,11 +2,14 @@ package com.vladmihalcea.book.hpjp.hibernate.jmx;
 
 import com.vladmihalcea.book.hpjp.util.AbstractTest;
 import org.hibernate.cfg.AvailableSettings;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * @author Vlad Mihalcea
@@ -21,19 +24,32 @@ public class JmxTest extends AbstractTest {
     }
 
     @Override
-    protected Properties properties() {
-        Properties properties = super.properties();
+    protected void additionalProperties(Properties properties) {
+        properties.put(AvailableSettings.GENERATE_STATISTICS, Boolean.TRUE.toString());
         properties.put(AvailableSettings.JMX_ENABLED, Boolean.TRUE.toString());
-        return properties;
+        properties.put("hibernate.cache.use_second_level_cache", Boolean.TRUE.toString());
+        properties.put("hibernate.cache.region.factory_class", "ehcache");
     }
 
+    private int seconds = 120;
+
     @Test
+    @Ignore
     public void test() {
-        doInJPA(entityManager -> {
-            Person vlad = new Person();
-            vlad.id = 1L;
-            entityManager.persist(vlad);
-        });
+        long startNanos = System.nanoTime();
+
+        AtomicLong id = new AtomicLong();
+
+        while (TimeUnit.NANOSECONDS.toSeconds(System.nanoTime() - startNanos) < seconds) {
+            doInJPA(entityManager -> {
+                if (Math.random() < 0.1) {
+                    Person person = new Person();
+                    person.setId(id.incrementAndGet());
+
+                    entityManager.persist(person);
+                }
+            });
+        }
     }
 
     @Entity(name = "Person")
@@ -45,5 +61,29 @@ public class JmxTest extends AbstractTest {
         private String firstName;
 
         private String lastName;
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
     }
 }
