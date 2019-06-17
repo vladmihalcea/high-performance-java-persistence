f8025e6cb63f80528ccc7b2e43406445b397f495
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2019-06-17 17:09:02 +0300

    Remove unneeded comments

--- core/src/test/java/com/vladmihalcea/book/hpjp/hibernate/logging/inspector/SqlCommentStatementInspector.java
+++ core/src/test/java/com/vladmihalcea/book/hpjp/hibernate/logging/inspector/SqlCommentStatementInspector.java
@@ -1,24 +1,3 @@
-/*
- * Copyright (c) 2019, Mihalcea Vlad-Alexandru (https://vladmihalcea.com)
- * All rights reserved.
- *
- * Mihalcea Vlad-Alexandru grants the Customer the non-exclusive,
- * timely limited and non-transferable license to install and use the Software
- * under the terms of the Hypersistence Optimizer License Agreement.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- *
- * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
- * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
- * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
- * See the Hypersistence Optimizer License Agreement for more details:
- *
- * https://vladmihalcea.com/hypersistence-optimizer/license
- */
 package com.vladmihalcea.book.hpjp.hibernate.logging.inspector;
 
 import org.hibernate.resource.jdbc.spi.StatementInspector;
