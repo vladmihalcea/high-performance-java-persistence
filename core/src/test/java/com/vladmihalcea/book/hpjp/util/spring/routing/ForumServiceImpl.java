e976d4490efe496041f9f2cc250a14573841282e
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2020-06-16 13:18:55 +0300

    Switch transaction routing service class to Text Blocks

--- core/src/test/java/com/vladmihalcea/book/hpjp/util/spring/routing/ForumServiceImpl.java
+++ core/src/test/java/com/vladmihalcea/book/hpjp/util/spring/routing/ForumServiceImpl.java
@@ -1,11 +1,13 @@
 package com.vladmihalcea.book.hpjp.util.spring.routing;
 
 import com.vladmihalcea.book.hpjp.hibernate.transaction.forum.Post;
+import com.vladmihalcea.book.hpjp.hibernate.transaction.forum.Tag;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -22,19 +24,31 @@ public class ForumServiceImpl implements ForumService {
     public Post newPost(String title, String... tags) {
         Post post = new Post();
         post.setTitle(title);
+
+        post.getTags().addAll(
+            entityManager.createQuery("""
+                select t
+                from Tag t
+                where t.name in :tags
+                """, Tag.class)
+            .setParameter("tags", Arrays.asList(tags))
+            .getResultList()
+        );
+
         entityManager.persist(post);
+
         return post;
     }
 
     @Override
     @Transactional(readOnly = true)
     public List<Post> findAllByTitle(String title) {
-        return entityManager.createQuery(
-            "select p " +
-                "from Post p " +
-                "where p.title = :title", Post.class)
-            .setParameter("title", title)
-            .getResultList();
+        return entityManager.createQuery("""
+            select p
+            from Post p
+            where p.title = :title
+            """, Post.class)
+        .setParameter("title", title)
+        .getResultList();
     }
-
 }
