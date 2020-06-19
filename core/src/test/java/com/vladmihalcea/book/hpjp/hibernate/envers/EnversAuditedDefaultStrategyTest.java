be1ff925f3d1bee05a71eab9baf51d5e640d75e3
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2020-06-19 12:24:07 +0300

    Arrange code

--- core/src/test/java/com/vladmihalcea/book/hpjp/hibernate/envers/EnversAuditedDefaultStrategyTest.java
+++ core/src/test/java/com/vladmihalcea/book/hpjp/hibernate/envers/EnversAuditedDefaultStrategyTest.java
@@ -1,23 +1,17 @@
 package com.vladmihalcea.book.hpjp.hibernate.envers;
 
-import java.util.List;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
+import com.vladmihalcea.book.hpjp.util.AbstractTest;
 import org.hibernate.envers.AuditReaderFactory;
 import org.hibernate.envers.Audited;
 import org.hibernate.envers.query.AuditEntity;
-
 import org.junit.Test;
 
-import com.vladmihalcea.book.hpjp.hibernate.equality.AbstractEqualityCheckTest;
-import com.vladmihalcea.book.hpjp.hibernate.identifier.Identifiable;
-import com.vladmihalcea.book.hpjp.util.AbstractTest;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 /**
  * @author Vlad Mihalcea
@@ -26,7 +20,7 @@ public class EnversAuditedDefaultStrategyTest extends AbstractTest {
 
     @Override
     protected Class<?>[] entities() {
-        return new Class[] {
+        return new Class[]{
             Post.class
         };
     }
@@ -35,50 +29,50 @@ public class EnversAuditedDefaultStrategyTest extends AbstractTest {
     public void test() {
         doInJPA(entityManager -> {
             Post post = new Post();
-            post.setId( 1L );
-            post.setTitle( "High-Performance Java Persistence 1st edition" );
-            entityManager.persist( post );
+            post.setId(1L);
+            post.setTitle("High-Performance Java Persistence 1st edition");
+            entityManager.persist(post);
         });
 
         doInJPA(entityManager -> {
-            Post post = entityManager.find( Post.class, 1L );
-            post.setTitle( "High-Performance Java Persistence 2nd edition" );
+            Post post = entityManager.find(Post.class, 1L);
+            post.setTitle("High-Performance Java Persistence 2nd edition");
         });
 
         doInJPA(entityManager -> {
             entityManager.remove(
-                entityManager.getReference( Post.class, 1L )
+                entityManager.getReference(Post.class, 1L)
             );
         });
 
         doInJPA(entityManager -> {
-            List<Post> posts = AuditReaderFactory.get( entityManager )
-            .createQuery()
-            .forRevisionsOfEntity( Post.class, true, true )
-            .add( AuditEntity.id().eq( 1L ) )
-            .getResultList();
+            List<Post> posts = AuditReaderFactory.get(entityManager)
+                .createQuery()
+                .forRevisionsOfEntity(Post.class, true, true)
+                .add(AuditEntity.id().eq(1L))
+                .getResultList();
 
-            assertEquals( 3, posts.size() );
+            assertEquals(3, posts.size());
 
-            for ( int i = 0; i < posts.size(); i++ ) {
-                LOGGER.info( "Revision {} of Post entity: {}", i + 1, posts.get( i ) );
+            for (int i = 0; i < posts.size(); i++) {
+                LOGGER.info("Revision {} of Post entity: {}", i + 1, posts.get(i));
             }
         });
 
-        List<Number> revisions = doInJPA( entityManager -> {
-            return AuditReaderFactory.get( entityManager ).getRevisions(
+        List<Number> revisions = doInJPA(entityManager -> {
+            return AuditReaderFactory.get(entityManager).getRevisions(
                 Post.class, 1L
             );
-        } );
+        });
 
-        doInJPA( entityManager -> {
-            Post post = (Post) AuditReaderFactory.get( entityManager )
-            .createQuery()
-            .forEntitiesAtRevision( Post.class, revisions.get( 0 ) )
-            .getSingleResult();
+        doInJPA(entityManager -> {
+            Post post = (Post) AuditReaderFactory.get(entityManager)
+                .createQuery()
+                .forEntitiesAtRevision(Post.class, revisions.get(0))
+                .getSingleResult();
 
             assertEquals("High-Performance Java Persistence 1st edition", post.getTitle());
-        } );
+        });
 
     }
 
@@ -111,9 +105,9 @@ public class EnversAuditedDefaultStrategyTest extends AbstractTest {
         @Override
         public String toString() {
             return "Post{" +
-                    "id=" + id +
-                    ", title='" + title + '\'' +
-                    '}';
+                   "id=" + id +
+                   ", title='" + title + '\'' +
+                   '}';
         }
     }
 }
