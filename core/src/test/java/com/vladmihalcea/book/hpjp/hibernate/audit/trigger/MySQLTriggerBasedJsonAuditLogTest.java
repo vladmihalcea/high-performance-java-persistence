0b310866d58bbab47e59db4dfe499decbf8f3fba
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2020-07-30 15:13:46 +0300

    Adjust test to match article

--- core/src/test/java/com/vladmihalcea/book/hpjp/hibernate/audit/trigger/MySQLTriggerBasedJsonAuditLogTest.java
+++ core/src/test/java/com/vladmihalcea/book/hpjp/hibernate/audit/trigger/MySQLTriggerBasedJsonAuditLogTest.java
@@ -4,6 +4,7 @@ import com.vladmihalcea.book.hpjp.util.AbstractTest;
 import com.vladmihalcea.book.hpjp.util.providers.Database;
 import com.vladmihalcea.hibernate.type.util.ReflectionUtils;
 import org.hibernate.Session;
+import org.hibernate.annotations.DynamicUpdate;
 import org.hibernate.dialect.Dialect;
 import org.hibernate.engine.spi.SessionFactoryImplementor;
 import org.junit.Test;
@@ -35,105 +36,105 @@ public class MySQLTriggerBasedJsonAuditLogTest extends AbstractTest {
         ddl("DROP TABLE IF EXISTS book_audit_log");
         ddl("""
             CREATE TABLE IF NOT EXISTS book_audit_log (
-                id BIGINT, 
+                book_id BIGINT NOT NULL, 
             	old_row_data JSON,
             	new_row_data JSON,
             	dml_type ENUM('INSERT', 'UPDATE', 'DELETE') NOT NULL,
             	dml_timestamp TIMESTAMP NOT NULL,
             	dml_created_by VARCHAR(255) NOT NULL,
-            	PRIMARY KEY (id, dml_type, dml_timestamp)
+            	PRIMARY KEY (book_id, dml_type, dml_timestamp)
             ) 
             """
         );
 
         ddl("""          
             CREATE TRIGGER book_insert_audit_trigger
-            AFTER INSERT ON book
-            FOR EACH ROW BEGIN
-            INSERT INTO book_audit_log (
-                id,
-                old_row_data,
-                new_row_data,
-                dml_type,
-                dml_timestamp,
-                dml_created_by
-            )
-            VALUES(
-                NEW.id,
-                null,
-                JSON_OBJECT(
-                    "title", NEW.title,
-                    "author", NEW.author,
-                    "price_in_cents", NEW.price_in_cents,
-                    "publisher", NEW.publisher
-                ),
-                'INSERT',
-                CURRENT_TIMESTAMP,
-                @logged_user
-            );
+            AFTER INSERT ON book FOR EACH ROW 
+            BEGIN
+                INSERT INTO book_audit_log (
+                    book_id,
+                    old_row_data,
+                    new_row_data,
+                    dml_type,
+                    dml_timestamp,
+                    dml_created_by
+                )
+                VALUES(
+                    NEW.id,
+                    null,
+                    JSON_OBJECT(
+                        "title", NEW.title,
+                        "author", NEW.author,
+                        "price_in_cents", NEW.price_in_cents,
+                        "publisher", NEW.publisher
+                    ),
+                    'INSERT',
+                    CURRENT_TIMESTAMP,
+                    @logged_user
+                );
             END
             """
         );
 
         ddl("""
             CREATE TRIGGER book_update_audit_trigger
-            AFTER UPDATE ON book
-            FOR EACH ROW BEGIN
-            INSERT INTO book_audit_log (
-                id,
-                old_row_data,
-                new_row_data,
-                dml_type,
-                dml_timestamp,
-                dml_created_by
-            )
-            VALUES(
-                NEW.id,
-                JSON_OBJECT(
-                    "title", OLD.title,
-                    "author", OLD.author,
-                    "price_in_cents", OLD.price_in_cents,
-                    "publisher", OLD.publisher
-                ),
-                JSON_OBJECT(
-                    "title", NEW.title,
-                    "author", NEW.author,
-                    "price_in_cents", NEW.price_in_cents,
-                    "publisher", NEW.publisher
-                ),
-                'UPDATE',
-                CURRENT_TIMESTAMP,
-                @logged_user
-            );
+            AFTER UPDATE ON book FOR EACH ROW 
+            BEGIN
+                INSERT INTO book_audit_log (
+                    book_id,
+                    old_row_data,
+                    new_row_data,
+                    dml_type,
+                    dml_timestamp,
+                    dml_created_by
+                )
+                VALUES(
+                    NEW.id,
+                    JSON_OBJECT(
+                        "title", OLD.title,
+                        "author", OLD.author,
+                        "price_in_cents", OLD.price_in_cents,
+                        "publisher", OLD.publisher
+                    ),
+                    JSON_OBJECT(
+                        "title", NEW.title,
+                        "author", NEW.author,
+                        "price_in_cents", NEW.price_in_cents,
+                        "publisher", NEW.publisher
+                    ),
+                    'UPDATE',
+                    CURRENT_TIMESTAMP,
+                    @logged_user
+                );
             END
             """
         );
 
         ddl("""
             CREATE TRIGGER book_delete_audit_trigger
-            AFTER DELETE ON book
-            FOR EACH ROW BEGIN
-            INSERT INTO book_audit_log (
-                id,
-                old_row_data,
-                new_row_data,
-                dml_type,
-                dml_timestamp,
-                dml_created_by
-            )
-            VALUES(
-                OLD.id,
-                JSON_OBJECT(
-                    "title", OLD.title,
-                    "author", OLD.author,
-                    "price_in_cents", OLD.price_in_cents,
-                    "publisher", OLD.publisher
-                ),
-                null,
-                'DELETE',
-                CURRENT_TIMESTAMP,
-                @logged_user
-            );
+            AFTER DELETE ON book FOR EACH ROW 
+            BEGIN
+                INSERT INTO book_audit_log (
+                    book_id,
+                    old_row_data,
+                    new_row_data,
+                    dml_type,
+                    dml_timestamp,
+                    dml_created_by
+                )
+                VALUES(
+                    OLD.id,
+                    JSON_OBJECT(
+                        "title", OLD.title,
+                        "author", OLD.author,
+                        "price_in_cents", OLD.price_in_cents,
+                        "publisher", OLD.publisher
+                    ),
+                    null,
+                    'DELETE',
+                    CURRENT_TIMESTAMP,
+                    @logged_user
+                );
             END
             """
         );
@@ -150,6 +151,8 @@ public class MySQLTriggerBasedJsonAuditLogTest extends AbstractTest {
                 new Book()
                     .setId(1L)
                     .setTitle("High-Performance Java Persistence 1st edition")
+                    .setPublisher("Amazon")
+                    .setPriceInCents(3990)
                     .setAuthor("Vlad Mihalcea")
             );
         });
@@ -164,7 +167,6 @@ public class MySQLTriggerBasedJsonAuditLogTest extends AbstractTest {
             setCurrentLoggedUser(entityManager);
 
             Book book = entityManager.find(Book.class, 1L)
-                .setPublisher("Amazon")
                 .setPriceInCents(4499);
         });
 
@@ -235,6 +237,7 @@ public class MySQLTriggerBasedJsonAuditLogTest extends AbstractTest {
 
     @Entity(name = "Book")
     @Table(name = "book")
+    @DynamicUpdate
     public static class Book {
 
         @Id
