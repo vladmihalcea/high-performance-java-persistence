c9a6dfa83c6e03436fafd5e8c9f3613a0b9615fd
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2020-08-25 09:07:54 +0300

    Use the same Flyway script names for both PostgreSQL and HSQLDB

--- core/src/test/resources/flyway/scripts/hsqldb/migration/V1_0__initial_script.sql
+++
@@ -1,4 +0,0 @@
-create sequence hibernate_sequence start with 1 increment by 1;
-
-create table post (id bigint not null, title varchar(255), primary key (id));
-create table tag (id bigint not null, name varchar(255), primary key (id));
--- core/src/test/resources/flyway/scripts/hsqldb/migration/V1_0__post_tag.sql
+++ core/src/test/resources/flyway/scripts/hsqldb/migration/V1_0__post_tag.sql
@@ -1,3 +1,8 @@
+create sequence hibernate_sequence start with 1 increment by 1;
+
+create table post (id bigint not null, title varchar(255), primary key (id));
+create table tag (id bigint not null, name varchar(255), primary key (id));
+
 create table post_tag (post_id bigint not null, tag_id bigint not null);
 
 alter table post_tag add constraint POST_TAG_TAG_ID_FK foreign key (tag_id) references tag (id);
