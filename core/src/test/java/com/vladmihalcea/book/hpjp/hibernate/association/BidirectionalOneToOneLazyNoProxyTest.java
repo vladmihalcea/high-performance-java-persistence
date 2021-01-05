b0484a195760c6e10b964f49722ac4ff2826195a
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2021-01-05 15:53:36 +0200

    Add a NoProxy test for OneToOne associations

--- core/src/test/java/com/vladmihalcea/book/hpjp/hibernate/association/BidirectionalOneToOneLazyNoProxyTest.java
+++ core/src/test/java/com/vladmihalcea/book/hpjp/hibernate/association/BidirectionalOneToOneLazyNoProxyTest.java
@@ -1,14 +1,16 @@
 package com.vladmihalcea.book.hpjp.hibernate.association;
 
-import com.vladmihalcea.book.hpjp.hibernate.forum.Post;
-import com.vladmihalcea.book.hpjp.hibernate.forum.PostComment;
-import com.vladmihalcea.book.hpjp.hibernate.forum.PostDetails;
-import com.vladmihalcea.book.hpjp.hibernate.forum.Tag;
 import com.vladmihalcea.book.hpjp.util.AbstractTest;
-import org.junit.Ignore;
+import org.hibernate.annotations.LazyToOne;
+import org.hibernate.annotations.LazyToOneOption;
+import org.hibernate.testing.bytecode.enhancement.BytecodeEnhancerRunner;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.List;
+import javax.persistence.*;
+import java.util.Date;
 
 import static junit.framework.TestCase.assertNull;
 import static junit.framework.TestCase.fail;
@@ -17,39 +19,40 @@ import static org.junit.Assert.assertNotNull;
 /**
  * @author Vlad Mihalcea
  */
+@RunWith(BytecodeEnhancerRunner.class)
 public class BidirectionalOneToOneLazyNoProxyTest extends AbstractTest {
+
+    //Needed as otherwise we get a No unique field [LOGGER] error
+    private final Logger LOGGER = LoggerFactory.getLogger(getClass());
 
     @Override
     protected Class<?>[] entities() {
         return new Class<?>[]{
             Post.class,
-            PostDetails.class,
-            PostComment.class,
-            Tag.class
+            PostDetails.class
         };
     }
 
     @Test
-    @Ignore
-    public void testNPlusOne() {
+    public void testLazyLoadingNoProxy() {
         doInJPA(entityManager -> {
-            Post post = new Post();
-            post.setId(1L);
-            post.setTitle("High-Performance Java Persistence, 1st Part");
-
-            PostDetails details = new PostDetails();
-            details.setCreatedBy("Vlad Mihalcea");
-
-            post.setDetails(details);
-
-            entityManager.persist(post);
+            entityManager.persist(
+                new Post()
+                    .setId(1L)
+                    .setTitle("High-Performance Java Persistence, 1st Part")
+                    .setDetails(
+                        new PostDetails()
+                            .setCreatedBy("Vlad Mihalcea")
+                    )
+            );
         });
 
         Post post = doInJPA(entityManager -> {
-            return entityManager.createQuery(
-                "select p " +
-                "from Post p " +
-                "where p.title like 'High-Performance Java Persistence%'", Post.class)
+            return entityManager.createQuery("""
+                select p
+                from Post p
+                where p.title like 'High-Performance Java Persistence%'
+                """, Post.class)
             .getSingleResult();
         });
 
@@ -62,27 +65,111 @@ public class BidirectionalOneToOneLazyNoProxyTest extends AbstractTest {
         }
     }
 
-    @Test
-    public void testLazyLoading() {
-        doInJPA(entityManager -> {
-            Post post = new Post();
-            post.setId(1L);
-            post.setTitle("High-Performance Java Persistence, 2nd Part");
+    @Entity(name = "Post")
+    @Table(name = "post")
+    public static class Post {
 
-            entityManager.persist(post);
-        });
+        @Id
+        private Long id;
 
-        doInJPA(entityManager -> {
-            Post post = entityManager.createQuery(
-                "select p " +
-                "from Post p " +
-                "where p.id = :postId", Post.class)
-            .setParameter("postId", 1L)
-            .getSingleResult();
+        private String title;
 
-            LOGGER.info("Fetched Post entity");
+        @OneToOne(
+            mappedBy = "post",
+            fetch = FetchType.LAZY,
+            cascade = CascadeType.ALL
+        )
+        @LazyToOne(LazyToOneOption.NO_PROXY)
+        private PostDetails details;
 
-            assertNull(post.getDetails());
-        });
+        public Long getId() {
+            return id;
+        }
+
+        public Post setId(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public Post setTitle(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public PostDetails getDetails() {
+            return details;
+        }
+
+        public Post setDetails(PostDetails details) {
+            if (details == null) {
+                if (this.details != null) {
+                    this.details.setPost(null);
+                }
+            }
+            else {
+                details.setPost(this);
+            }
+            this.details = details;
+            return this;
+        }
+    }
+
+    @Entity(name = "PostDetails")
+    @Table(name = "post_details")
+    public static class PostDetails {
+
+        @Id
+        @GeneratedValue
+        private Long id;
+
+        @Column(name = "created_on")
+        private Date createdOn = new Date();
+
+        @Column(name = "created_by")
+        private String createdBy;
+
+        @OneToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "post_id")
+        private Post post;
+
+        public Long getId() {
+            return id;
+        }
+
+        public PostDetails setId(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Post getPost() {
+            return post;
+        }
+
+        public PostDetails setPost(Post post) {
+            this.post = post;
+            return this;
+        }
+
+        public Date getCreatedOn() {
+            return createdOn;
+        }
+
+        public PostDetails setCreatedOn(Date createdOn) {
+            this.createdOn = createdOn;
+            return this;
+        }
+
+        public String getCreatedBy() {
+            return createdBy;
+        }
+
+        public PostDetails setCreatedBy(String createdBy) {
+            this.createdBy = createdBy;
+            return this;
+        }
     }
 }
