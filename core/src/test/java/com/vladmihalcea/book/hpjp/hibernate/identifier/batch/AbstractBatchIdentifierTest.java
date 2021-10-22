a03475ec9e21d4157f88dd8fe9c9a16bc0ea175a
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2021-10-22 22:03:50 +0300

    Add Batch Sequence Generator Test

--- core/src/test/java/com/vladmihalcea/book/hpjp/hibernate/identifier/batch/AbstractBatchIdentifierTest.java
+++ core/src/test/java/com/vladmihalcea/book/hpjp/hibernate/identifier/batch/AbstractBatchIdentifierTest.java
@@ -7,12 +7,10 @@ import java.util.Properties;
 public abstract class AbstractBatchIdentifierTest extends AbstractTest {
 
     @Override
-    protected Properties properties() {
-        Properties properties = super.properties();
+    protected void additionalProperties(Properties properties) {
         properties.put("hibernate.order_inserts", "true");
         properties.put("hibernate.order_updates", "true");
         properties.put("hibernate.jdbc.batch_size", "2");
-        return properties;
     }
 
 }
---
+++ core/src/test/java/com/vladmihalcea/book/hpjp/hibernate/identifier/batch/BatchSequenceIdentifierTest.java
@@ -0,0 +1,108 @@
+package com.vladmihalcea.book.hpjp.hibernate.identifier.batch;
+
+import com.vladmihalcea.book.hpjp.util.providers.Database;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+import org.junit.Test;
+
+import javax.persistence.*;
+import java.util.Properties;
+
+public class BatchSequenceIdentifierTest extends AbstractBatchIdentifierTest {
+
+    private static final int POST_SIZE = 10;
+    private static final int BATCH_SIZE = 5;
+
+    @Override
+    protected Class<?>[] entities() {
+        return new Class<?>[] {
+            Post.class,
+        };
+    }
+
+    @Override
+    protected Database database() {
+        return Database.POSTGRESQL;
+    }
+
+    @Override
+    protected void additionalProperties(Properties properties) {
+        properties.put("hibernate.jdbc.batch_size", "5");
+    }
+
+    @Test
+    public void testSequenceIdentifierGenerator() {
+        executeStatement("DROP SEQUENCE IF EXISTS post_sequence");
+        executeStatement("""
+            CREATE SEQUENCE post_sequence
+            INCREMENT BY 1
+            START WITH 1
+            CACHE 5
+            """);
+
+        doInJPA(entityManager -> {
+            for (int i = 1; i <= POST_SIZE; i++) {
+                entityManager.persist(
+                    new Post()
+                        .setTitle(
+                            String.format(
+                                "High-Performance Java Persistence, Chapter %d",
+                                i
+                            )
+                        )
+                );
+                if(i % BATCH_SIZE == 0) {
+                    EntityTransaction entityTransaction = entityManager.getTransaction();
+                    entityTransaction.commit();
+                    entityTransaction.begin();
+                    entityManager.clear();
+                }
+            }
+        });
+    }
+
+    @Entity(name = "Post")
+    @Table(name = "post")
+    public static class Post {
+
+        public static final String BATCH_SEQUENCE_GENERATOR =
+            "com.github.marschall.hibernate.batchsequencegenerator.BatchSequenceGenerator";
+
+        @Id
+        @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "post_sequence"
+        )
+        //Uncomment to see the default JPA sequence logic
+        /*@SequenceGenerator(name = "post_sequence", allocationSize = 1)*/
+        @GenericGenerator(
+            name = "post_sequence",
+            strategy = BATCH_SEQUENCE_GENERATOR,
+            parameters = {
+                @Parameter(name = "sequence", value = "post_sequence"),
+                @Parameter(name = "fetch_size", value = "5")
+            }
+        )
+        private Long id;
+
+        private String title;
+
+        public Long getId() {
+            return id;
+        }
+
+        public Post setId(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public Post setTitle(String title) {
+            this.title = title;
+            return this;
+        }
+    }
+}
