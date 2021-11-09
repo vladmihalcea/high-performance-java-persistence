cfcbf99cee30991e3a64fec62708fcbc31a2f766
Ivan Zaitsev <ivan909020@gmail.com>
2021-11-09 18:46:03 +0200

    Add tests for delete parent entity with @OneToMany cascade = ALL

---
+++ core/src/test/java/com/vladmihalcea/book/hpjp/hibernate/fetching/deleting/AbstractDeletingCheckTest.java
@@ -0,0 +1,224 @@
+package com.vladmihalcea.book.hpjp.hibernate.fetching.deleting;
+
+import com.vladmihalcea.book.hpjp.util.AbstractTest;
+import com.vladmihalcea.book.hpjp.util.providers.Database;
+import com.vladmihalcea.book.hpjp.util.transaction.JPATransactionVoidFunction;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Vlad Mihalcea
+ */
+public class AbstractDeletingCheckTest extends AbstractTest {
+
+    @Override
+    protected Class<?>[] entities() {
+        return new Class<?>[]{
+                Audience.class,
+                Group.class,
+                Lesson.class
+        };
+    }
+
+    @Override
+    protected Database database() {
+        return Database.POSTGRESQL;
+    }
+
+    protected void doInJPA(JPATransactionVoidFunction function, EntityManager entityManager) {
+        EntityTransaction txn = null;
+        try {
+            function.beforeTransactionCompletion();
+            txn = entityManager.getTransaction();
+            txn.begin();
+            function.accept(entityManager);
+            if ( !txn.getRollbackOnly() ) {
+                txn.commit();
+            }
+            else {
+                try {
+                    txn.rollback();
+                }
+                catch (Exception e) {
+                    LOGGER.error( "Rollback failure", e );
+                }
+            }
+        } catch (Throwable t) {
+            if ( txn != null && txn.isActive() ) {
+                try {
+                    txn.rollback();
+                }
+                catch (Exception e) {
+                    LOGGER.error( "Rollback failure", e );
+                }
+            }
+            throw t;
+        } finally {
+            function.afterTransactionCompletion();
+        }
+    }
+
+    protected void createTables() {
+        executeStatement("DROP TABLE IF EXISTS lessons");
+        executeStatement("DROP TABLE IF EXISTS audiences");
+        executeStatement("DROP TABLE IF EXISTS groups");
+
+        executeStatement("""
+                CREATE TABLE groups(
+                    id   BIGINT PRIMARY KEY,
+                    name VARCHAR
+                )
+                """);
+        executeStatement("""
+                CREATE TABLE audiences(
+                    id     BIGINT PRIMARY KEY,
+                    number INTEGER
+                )
+                """);
+    }
+
+    protected void insertData() {
+        executeStatement("INSERT INTO groups(id, name) VALUES (1, '1')");
+        executeStatement("INSERT INTO audiences(id, number) VALUES (1, 1), (2, 2)");
+        executeStatement("INSERT INTO lessons(id, audience_id, group_id) VALUES (1, 1, 1)");
+    }
+
+    @Entity(name = "Audience")
+    @Table(name = "audiences")
+    public static class Audience {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.SEQUENCE)
+        private Long id;
+
+        @Column
+        private Integer number;
+
+        @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "audience")
+        private List<Lesson> lessons = new ArrayList<>();
+
+        public Audience() {}
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public Integer getNumber() {
+            return number;
+        }
+
+        public void setNumber(Integer number) {
+            this.number = number;
+        }
+
+        public List<Lesson> getLessons() {
+            return lessons;
+        }
+
+        public void setLessons(List<Lesson> lessons) {
+            this.lessons = lessons;
+        }
+    }
+
+    @Entity(name = "Group")
+    @Table(name = "groups")
+    public static class Group {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.SEQUENCE)
+        private Long id;
+
+        @Column
+        private String name;
+
+        @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "group")
+        private List<Lesson> lessons = new ArrayList<>();
+
+        public Group() {}
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public List<Lesson> getLessons() {
+            return lessons;
+        }
+
+        public void setLessons(List<Lesson> lessons) {
+            this.lessons = lessons;
+        }
+    }
+
+    @Entity(name = "Lesson")
+    @Table(name = "lessons")
+    public static class Lesson {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.SEQUENCE)
+        private Long id;
+
+        @ManyToOne
+        @JoinColumn(name = "audience_id")
+        private Audience audience;
+
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "group_id")
+        private Group group;
+
+        public Lesson() {}
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public Audience getAudience() {
+            return audience;
+        }
+
+        public void setAudience(Audience audience) {
+            this.audience = audience;
+        }
+
+        public Group getGroup() {
+            return group;
+        }
+
+        public void setGroup(Group group) {
+            this.group = group;
+        }
+    }
+}
---
+++ core/src/test/java/com/vladmihalcea/book/hpjp/hibernate/fetching/deleting/DeletingAfterFetchWithoutSQLCascadeTest.java
@@ -0,0 +1,97 @@
+package com.vladmihalcea.book.hpjp.hibernate.fetching.deleting;
+
+import org.hibernate.exception.ConstraintViolationException;
+import org.junit.Test;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+/**
+ * @author Vlad Mihalcea
+ */
+public class DeletingAfterFetchWithoutSQLCascadeTest extends AbstractDeletingCheckTest {
+
+    @Test
+    public void testDeleteParentAfterEagerInnerFetchInChildWithoutSQLCascadeInOneTransaction() {
+        createTables();
+        insertData();
+
+        doInJPA(entityManager -> {
+            entityManager.createQuery("""
+                SELECT a FROM Audience a
+                    LEFT JOIN FETCH a.lessons l
+                    LEFT JOIN FETCH l.group g
+                    WHERE a.id = :audienceId
+                """, Audience.class)
+            .setParameter( "audienceId", 1L )
+            .getSingleResult();
+
+            Lesson lesson = entityManager.find(Lesson.class, 1L);
+            entityManager.remove(lesson);
+            entityManager.flush();
+
+            try {
+                Audience audience = entityManager.find(Audience.class, 1L);
+                entityManager.remove(audience);
+                entityManager.flush();
+
+                fail("Should throw ConstraintViolationException");
+            } catch (Exception expected) {
+                assertEquals(ConstraintViolationException.class, expected.getCause().getClass());
+                LOGGER.info("Failure: ", expected);
+            }
+        });
+    }
+
+    @Test
+    public void testDeleteParentAfterEagerInnerFetchInChildWithoutSQLCascadeThenSaveAnotherEntityInSeveralTransactions() {
+        createTables();
+        insertData();
+
+        EntityManager _entityManager = entityManagerFactory().createEntityManager();
+
+        doInJPA(entityManager -> {
+            entityManager.createQuery("""
+                SELECT a FROM Audience a
+                    LEFT JOIN FETCH a.lessons l
+                    LEFT JOIN FETCH l.group g
+                    WHERE a.id = :audienceId
+                """, Audience.class)
+            .setParameter( "audienceId", 1L )
+            .getSingleResult();
+        }, _entityManager);
+
+        doInJPA(entityManager -> {
+            Lesson lesson = entityManager.find(Lesson.class, 1L);
+            entityManager.remove(lesson);
+        }, _entityManager);
+
+        try {
+            doInJPA(entityManager -> {
+                Audience audience = entityManager.find(Audience.class, 1L);
+                entityManager.remove(audience);
+            }, _entityManager);
+
+            fail("Should throw ConstraintViolationException");
+        } catch (Exception expected) {
+            assertEquals(ConstraintViolationException.class, expected.getCause().getCause().getClass());
+            LOGGER.info("Failure: ", expected);
+        } finally {
+            _entityManager.close();
+        }
+    }
+
+    @Override
+    protected void createTables() {
+        super.createTables();
+        executeStatement("""
+                CREATE TABLE lessons(
+                    id          BIGINT PRIMARY KEY,
+                    audience_id INTEGER NOT NULL REFERENCES audiences (id),
+                    group_id    INTEGER NOT NULL REFERENCES groups (id)
+                )
+                """);
+    }
+}
