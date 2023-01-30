899b5c73fccd591b38db611f2166b5fe5e6ac395
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2023-01-30 18:10:30 +0200

    Simplify test

--- core/src/test/java/com/vladmihalcea/book/hpjp/hibernate/statistics/ConnectionStatisticsTest.java
+++ core/src/test/java/com/vladmihalcea/book/hpjp/hibernate/statistics/ConnectionStatisticsTest.java
@@ -2,21 +2,18 @@ package com.vladmihalcea.book.hpjp.hibernate.statistics;
 
 import com.vladmihalcea.book.hpjp.util.AbstractTest;
 import com.vladmihalcea.book.hpjp.util.providers.entity.BlogEntityProvider;
+import jakarta.persistence.*;
 import org.hibernate.Session;
 import org.hibernate.cfg.AvailableSettings;
-import org.hibernate.engine.jdbc.connections.internal.DatasourceConnectionProviderImpl;
 import org.hibernate.resource.jdbc.spi.PhysicalConnectionHandlingMode;
 import org.hibernate.stat.Statistics;
 import org.hibernate.stat.internal.StatisticsInitiator;
 import org.junit.Test;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
-import java.util.concurrent.atomic.AtomicBoolean;
 
-import static com.vladmihalcea.book.hpjp.util.providers.entity.BlogEntityProvider.Post;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -27,63 +24,12 @@ public class ConnectionStatisticsTest extends AbstractTest {
 
     @Override
     protected Class<?>[] entities() {
-        return BlogEntityProvider.INSTANCE.entities();
-    }
-
-    private final AtomicBoolean autoCommit = new AtomicBoolean(true);
-
-    private class ThreadLocalDatasourceConnectionProvider
-            extends DatasourceConnectionProviderImpl {
-
-        public ThreadLocalDatasourceConnectionProvider(final DataSource dataSource) {
-            setDataSource(dataSource);
-        }
-
-        final ThreadLocal<Connection> connectionHolder = new ThreadLocal<>();
-
-        @Override
-        public Connection getConnection()
-                throws SQLException {
-            Connection connection = connectionHolder.get();
-            if(connection == null) {
-                connection = super.getConnection();
-                if (!autoCommit.get()) {
-                    connection.setAutoCommit(false);
-                }
-                connectionHolder.set(connection);
-            }
-            return connection;
-        }
-
-        @Override
-        public void closeConnection(Connection connection)
-                throws SQLException {
-            if (autoCommit.get()) {
-                super.closeConnection(connection);
-                connectionHolder.remove();
-            }
-        }
+        return new Class[] {
+            Post.class
+        };
     }
 
     protected void additionalProperties(Properties properties) {
-        DataSource actualDataSource = (DataSource) properties
-            .get(AvailableSettings.DATASOURCE);
-
-        properties.put(
-            AvailableSettings.CONNECTION_PROVIDER,
-            new ThreadLocalDatasourceConnectionProvider(actualDataSource)
-        );
-
-        properties.put(
-            AvailableSettings.CONNECTION_HANDLING,
-            PhysicalConnectionHandlingMode.DELAYED_ACQUISITION_AND_RELEASE_AFTER_STATEMENT
-        );
-
-        properties.put(
-            AvailableSettings.CONNECTION_PROVIDER_DISABLES_AUTOCOMMIT,
-            Boolean.TRUE.toString()
-        );
-
         properties.put(
             AvailableSettings.GENERATE_STATISTICS,
             Boolean.TRUE.toString()
@@ -99,30 +45,22 @@ public class ConnectionStatisticsTest extends AbstractTest {
     public void test() {
         int iterations = 5;
 
-        for (long i = 0; i < iterations; i++) {
-            autoCommit.set(false);
-            final Long id = i + 1;
-
+        for (long i = 1; i <= iterations; i++) {
+            final long currentIteration = i;
             doInJPA(entityManager -> {
-                try {
-                    Post post = new Post();
-                    post.setId(id);
-                    post.setTitle(
-                        String.format(
-                            "High-Performance Java Persistence, Part %d", id
-                        )
-                    );
-                    entityManager.persist(post);
+                Post post = new Post();
+                post.setTitle(
+                    String.format(
+                        "High-Performance Java Persistence, Part %d", currentIteration
+                    )
+                );
+                entityManager.persist(post);
 
-                    Number postCount = entityManager.createQuery(
-                        "select count(p) from Post p", Number.class)
+                Number postCount = entityManager.createQuery(
+                    "select count(p) from Post p", Number.class)
                     .getSingleResult();
 
-                    assertEquals((long) id, postCount.longValue());
-                }
-                finally {
-                    autoCommit.set(true);
-                }
+                assertEquals(currentIteration, postCount.longValue());
             });
         }
     }
@@ -135,5 +73,33 @@ public class ConnectionStatisticsTest extends AbstractTest {
             Statistics statistics = session.getSessionFactory().getStatistics();
             assertTrue(statistics instanceof TransactionStatistics);
         });
+    }
+
+    @Entity(name = "Post")
+    @Table(name = "post")
+    public static class Post {
+
+        @Id
+        //To get an extra connection
+        @GeneratedValue(strategy = GenerationType.TABLE)
+        private Long id;
+
+        private String title;
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
     }
 }
