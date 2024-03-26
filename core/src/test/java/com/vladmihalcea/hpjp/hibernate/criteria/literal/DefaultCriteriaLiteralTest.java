139e66e01a4fb57d7b912ee8a3e11ae058e47e7c
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2024-03-26 16:02:35 +0200

    Match examples with slides

--- core/src/test/java/com/vladmihalcea/hpjp/hibernate/criteria/literal/DefaultCriteriaLiteralTest.java
+++ core/src/test/java/com/vladmihalcea/hpjp/hibernate/criteria/literal/DefaultCriteriaLiteralTest.java
@@ -2,15 +2,15 @@ package com.vladmihalcea.hpjp.hibernate.criteria.literal;
 
 import com.vladmihalcea.hpjp.util.AbstractTest;
 import com.vladmihalcea.hpjp.util.providers.Database;
-import org.hibernate.annotations.NaturalId;
-import org.junit.Test;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.ParameterExpression;
 import jakarta.persistence.criteria.Root;
+import org.hibernate.annotations.NaturalId;
+import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
@@ -48,10 +48,25 @@ public class DefaultCriteriaLiteralTest extends AbstractTest {
 
             CriteriaQuery<Book> cq = cb.createQuery(Book.class);
             Root<Book> root = cq.from(Book.class);
-            cq.select(root);
-            cq.where(cb.equal(root.get("isbn"), 978_9730228236L));
 
-            Book book = entityManager.createQuery(cq).getSingleResult();
+            Book book = entityManager.createQuery(cq
+                    .select(root)
+                    .where(cb.equal(root.get("name"), "High-Performance Java Persistence")))
+                .getSingleResult();
+            assertEquals("High-Performance Java Persistence", book.getName());
+            assertEquals(978_9730228236L, book.getIsbn());
+        });
+
+        doInJPA(entityManager -> {
+            CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+
+            CriteriaQuery<Book> cq = cb.createQuery(Book.class);
+            Root<Book> root = cq.from(Book.class);
+
+            Book book = entityManager.createQuery(cq
+                    .select(root)
+                    .where(cb.equal(root.get("isbn"), 978_9730228236L)))
+                .getSingleResult();
             assertEquals("High-Performance Java Persistence", book.getName());
         });
 
@@ -60,11 +75,16 @@ public class DefaultCriteriaLiteralTest extends AbstractTest {
 
             CriteriaQuery<Book> cq = cb.createQuery(Book.class);
             Root<Book> root = cq.from(Book.class);
-            cq.select(root);
-            cq.where(cb.equal(root.get("name"), "High-Performance Java Persistence"));
+            ParameterExpression<Long> isbn = cb.parameter(Long.class);
+            cq.select(root)
+                .where(cb.equal(root.get("isbn"), isbn));
 
-            Book book = entityManager.createQuery(cq).getSingleResult();
-            assertEquals(978_9730228236L, book.getIsbn());
+            Book book = entityManager.createQuery(cq
+                .select(root)
+                .where(cb.equal(root.get("isbn"), isbn)))
+            .setParameter(isbn, 978_9730228236L)
+            .getSingleResult();
+            assertEquals("High-Performance Java Persistence", book.getName());
         });
 
         doInJPA(entityManager -> {
