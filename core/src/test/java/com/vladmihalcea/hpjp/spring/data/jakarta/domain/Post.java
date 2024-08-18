7ca3f2d5c5089435bf85eb346549f61bb6970eb2
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2024-08-18 13:42:00 +0300

    Add Jakarta Data example

---
+++ core/src/test/java/com/vladmihalcea/hpjp/spring/data/jakarta/domain/Post.java
@@ -0,0 +1,57 @@
+package com.vladmihalcea.hpjp.spring.data.jakarta.domain;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Vlad Mihalcea
+ */
+@Entity(name = "Post")
+@Table(name = "post")
+public class Post {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String title;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<PostComment> comments = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public Post setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Post setTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public List<PostComment> getComments() {
+        return comments;
+    }
+
+    public Post addComment(PostComment comment) {
+        comments.add(comment);
+        comment.setPost(this);
+        return this;
+    }
+
+    public Post removeComment(PostComment comment) {
+        comments.remove(comment);
+        comment.setPost(null);
+        return this;
+    }
+}
