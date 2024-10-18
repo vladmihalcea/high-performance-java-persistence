217528cf0d07bcaa44d1d1c494de6aad4e638c95
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2024-10-18 15:35:29 +0300

    Change group_concat test case

--- core/src/test/java/com/vladmihalcea/hpjp/hibernate/query/function/GroupConcatFunctionTest.java
+++ core/src/test/java/com/vladmihalcea/hpjp/hibernate/query/function/GroupConcatFunctionTest.java
@@ -1,6 +1,9 @@
 package com.vladmihalcea.hpjp.hibernate.query.function;
 
 import com.vladmihalcea.hpjp.util.AbstractMySQLIntegrationTest;
+import jakarta.persistence.criteria.*;
+import org.hibernate.query.Query;
+import org.hibernate.transform.Transformers;
 import org.junit.Test;
 
 import jakarta.persistence.*;
@@ -50,15 +53,71 @@ public class GroupConcatFunctionTest extends AbstractMySQLIntegrationTest {
     @Test
     public void testGroupConcatNaiveQuery() {
         doInJPA(entityManager -> {
-            List<Object[]> postSummaries = entityManager.createNativeQuery(
-                    "select p.id, p.title, group_concat(t.name) " +
-                            "from Post p " +
-                            "left join post_tag pt on p.id = pt.post_id " +
-                            "left join tag t on t.id = pt.tag_id " +
-                            "group by p.id, p.title")
-                    .getResultList();
+            List<Object[]> postSummaries = entityManager.createNativeQuery("""
+                select p.id, p.title, group_concat(t.name)
+                from Post p
+                left join post_tag pt on p.id = pt.post_id
+                left join tag t on t.id = pt.tag_id
+                group by p.id, p.title
+                """)
+            .getResultList();
 
             assertEquals(1, postSummaries.size());
+        });
+    }
+
+    @Test
+    public void testGroupConcatJPQLQuery() {
+        doInJPA(entityManager -> {
+            List<PostSummaryDTO> postSummaries = entityManager.createQuery("""
+                select
+                   p.id as id,
+                   p.title as title,
+                   group_concat(t.name) as tags
+                from Post p
+                left join p.tags t
+                group by p.id, p.title
+                """)
+            .unwrap(Query.class)
+            .setTupleTransformer(Transformers.aliasToBean(PostSummaryDTO.class))
+            .getResultList();
+
+            assertEquals(1, postSummaries.size());
+            LOGGER.info("Post tags: {}", postSummaries.get(0).getTags());
+        });
+    }
+
+    @Test
+    public void testGroupConcatCriteriaAPIQuery() {
+        doInJPA(entityManager -> {
+            CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+
+            CriteriaQuery<PostSummaryDTO> cq = cb.createQuery(
+                PostSummaryDTO.class
+            );
+
+            Root<Post> post = cq.from(Post.class);
+            Join tags = post.join("tags", JoinType.LEFT);
+            cq.groupBy(post.get("id"), post.get("title"));
+
+            cq.select(
+                cb.construct(
+                    PostSummaryDTO.class,
+                    post.get("id"),
+                    post.get("title"),
+                    cb.function(
+                        "group_concat",
+                        String.class,
+                        tags.get("name")
+                    )
+                )
+            );
+
+            List<PostSummaryDTO> postSummaries = entityManager.createQuery(cq)
+                .getResultList();
+
+            assertEquals(1, postSummaries.size());
+            LOGGER.info("Post tags: {}", postSummaries.get(0).getTags());
         });
     }
 
