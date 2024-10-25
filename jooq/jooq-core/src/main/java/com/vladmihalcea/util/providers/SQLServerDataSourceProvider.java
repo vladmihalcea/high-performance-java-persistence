44a588ce7e1e9105e84047e038ed5729b1c8c3d6
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2024-10-25 11:01:43 +0300

    Add documentation for SQL Server DataSourceProvider

--- jooq/jooq-core/src/main/java/com/vladmihalcea/util/providers/SQLServerDataSourceProvider.java
+++ jooq/jooq-core/src/main/java/com/vladmihalcea/util/providers/SQLServerDataSourceProvider.java
@@ -7,6 +7,8 @@ import javax.sql.DataSource;
 import java.util.Properties;
 
 /**
+ * SQLServerDataSourceProvider - Database connection properties for SQL Server
+ *
  * @author Vlad Mihalcea
  */
 public class SQLServerDataSourceProvider extends AbstractContainerDataSourceProvider {
