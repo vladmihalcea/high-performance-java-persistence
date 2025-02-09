bf577f8c3939a8f80446f8966c60b23a2df1913c
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2025-02-09 12:48:54 +0200

    Customize the MySQL index selectivity test

--- core/src/test/java/com/vladmihalcea/hpjp/jdbc/index/MySQLIndexSelectivityTest.java
+++ core/src/test/java/com/vladmihalcea/hpjp/jdbc/index/MySQLIndexSelectivityTest.java
@@ -1,9 +1,10 @@
 package com.vladmihalcea.hpjp.jdbc.index;
 
-import com.vladmihalcea.hpjp.util.AbstractPostgreSQLIntegrationTest;
+import com.vladmihalcea.hpjp.util.AbstractTest;
+import com.vladmihalcea.hpjp.util.providers.DataSourceProvider;
 import com.vladmihalcea.hpjp.util.providers.Database;
+import com.vladmihalcea.hpjp.util.providers.MySQLDataSourceProvider;
 import jakarta.persistence.*;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.sql.Connection;
@@ -17,7 +18,6 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.fail;
 
 /**
@@ -25,7 +25,7 @@ import static org.junit.Assert.fail;
  *
  * @author Vlad Mihalcea
  */
-public class MySQLIndexSelectivityTest extends AbstractPostgreSQLIntegrationTest {
+public class MySQLIndexSelectivityTest extends AbstractTest {
 
     public static final String INSERT_TASK = "INSERT INTO task (id, name, status) VALUES (?, ?, ?)";
 
@@ -37,12 +37,12 @@ public class MySQLIndexSelectivityTest extends AbstractPostgreSQLIntegrationTest
     }
 
     @Override
-    protected Database database() {
-        return Database.MYSQL;
+    protected DataSourceProvider dataSourceProvider() {
+        return new MySQLDataSourceProvider()
+            .setRewriteBatchedStatements(true);
     }
 
     @Test
-    @Ignore("Requires changes")
     public void testInsert() {
         AtomicInteger statementCount = new AtomicInteger();
         long startNanos = System.nanoTime();
@@ -52,9 +52,9 @@ public class MySQLIndexSelectivityTest extends AbstractPostgreSQLIntegrationTest
 
                 for (int i = 0; i < taskCount; i++) {
                     String status = Task.Status.DONE.name();
-                    if (i > 99000) {
+                    if (i >= (0.99 * taskCount)) {
                         status = Task.Status.TO_DO.name();
-                    } else if (i > 95000) {
+                    } else if (i >= (0.95 * taskCount)) {
                         status = Task.Status.FAILED.name();
                     }
                     statement.setLong(1, i);
@@ -70,12 +70,19 @@ public class MySQLIndexSelectivityTest extends AbstractPostgreSQLIntegrationTest
         LOGGER.info("{}.testInsert took {} millis",
                 getClass().getSimpleName(),
                 TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - startNanos));
-        executeStatement("CREATE INDEX idx_task_status ON task (status)");
-        executeStatement("OPTIMIZE TABLE task");
+
+        boolean createIndex = false;
+
+        if (createIndex) {
+            executeStatement("CREATE INDEX idx_task_status ON task (status)");
+            executeStatement("OPTIMIZE TABLE task");
+        }
+
+        boolean printExecutionPlans = false;
 
         doInJDBC(connection -> {
-            printExecutionPlanForSelectByStatus(connection, Task.Status.DONE);
-            printExecutionPlanForSelectByStatus(connection, Task.Status.TO_DO);
+            executeQueryWithSkewedFilterPredicate(connection, Task.Status.DONE, printExecutionPlans);
+            executeQueryWithSkewedFilterPredicate(connection, Task.Status.TO_DO, printExecutionPlans);
         });
     }
 
@@ -88,32 +95,45 @@ public class MySQLIndexSelectivityTest extends AbstractPostgreSQLIntegrationTest
     }
 
     protected int getTaskCount() {
-        return 250 * 1000;
+        return 10_000 * 1_000;
     }
 
     protected int getBatchSize() {
-        return 100;
+        return 1000;
     }
 
-    private void printExecutionPlanForSelectByStatus(Connection connection, Task.Status status) throws SQLException {
-        try (PreparedStatement statement = connection.prepareStatement("""
-                EXPLAIN FORMAT=JSON
-                SELECT *
-                FROM task
-                WHERE status = ?
-                """
-        )) {
+    private void executeQueryWithSkewedFilterPredicate(Connection connection, Task.Status status, boolean printExecutionPlans) throws SQLException {
+        String query = String.format("""
+            %sSELECT id, name
+            FROM task
+            WHERE status = ?
+            """,
+            printExecutionPlans ? "EXPLAIN FORMAT=JSON " : ""
+        );
+
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
             statement.setString(1, status.name());
             ResultSet resultSet = statement.executeQuery();
 
-            List<String> planLines = new ArrayList<>();
-            while (resultSet.next()) {
-                planLines.add(resultSet.getString(1));
+            if (printExecutionPlans) {
+                List<String> planLines = new ArrayList<>();
+                while (resultSet.next()) {
+                    planLines.add(resultSet.getString(1));
+                }
+                LOGGER.info("Execution plan: {}{}",
+                    System.lineSeparator(),
+                    planLines.stream().collect(Collectors.joining(System.lineSeparator()))
+                );
+            } else {
+                int rowCount = 0;
+                while (resultSet.next()) {
+                    rowCount++;
+                }
+                LOGGER.info("Fetched [{}] records for [{}] status",
+                    rowCount,
+                    status
+                );
             }
-            LOGGER.info("Execution plan: {}{}",
-                System.lineSeparator(),
-                planLines.stream().collect(Collectors.joining(System.lineSeparator()))
-            );
         }
     }
 
