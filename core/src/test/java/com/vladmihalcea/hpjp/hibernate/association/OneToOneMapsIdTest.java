6b5461fc886bb0724361fe7083801ae8c08d75e8
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2025-11-07 19:45:02 +0200

    Add a test case to demonstrate how to fetch both the parent and the child when using a unidirectional one-to-one relationship

--- core/src/test/java/com/vladmihalcea/hpjp/hibernate/association/OneToOneMapsIdTest.java
+++ core/src/test/java/com/vladmihalcea/hpjp/hibernate/association/OneToOneMapsIdTest.java
@@ -2,12 +2,17 @@ package com.vladmihalcea.hpjp.hibernate.association;
 
 import com.vladmihalcea.hpjp.util.AbstractTest;
 import com.vladmihalcea.hpjp.util.providers.Database;
+import io.hypersistence.utils.hibernate.type.util.ClassImportIntegrator;
+import jakarta.persistence.*;
+import org.hibernate.jpa.boot.spi.IntegratorProvider;
 import org.junit.Test;
 
-import jakarta.persistence.*;
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
+import java.util.Properties;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 /**
  * @author Vlad Mihalcea
@@ -24,20 +29,68 @@ public class OneToOneMapsIdTest extends AbstractTest {
 
     @Override
     protected Database database() {
-        return Database.MYSQL;
+        return Database.POSTGRESQL;
+    }
+
+    @Override
+    protected void additionalProperties(Properties properties) {
+        properties.put(
+            "hibernate.integrator_provider",
+            (IntegratorProvider) () -> Collections.singletonList(
+                new ClassImportIntegrator(
+                    List.of(
+                        PostWithDetailsRecord.class
+                    )
+                )
+            )
+        );
     }
 
     @Test
     public void testLifecycle() {
         doInJPA(entityManager -> {
-            Post post = new Post("First post");
-            entityManager.persist(post);
+            entityManager.persist(
+                new Post()
+                    .setId(1L)
+                    .setTitle("First post")
+            );
         });
+
+        doInJPA(entityManager -> {
+            PostWithDetailsRecord postWithDetailsRecord = entityManager.createQuery("""
+                select new PostWithDetailsRecord(p, pd)
+                from Post p
+                left join PostDetails pd on pd.id = p.id
+                where p.id = :postId
+                """,
+                PostWithDetailsRecord.class)
+            .setParameter("postId", 1L)
+            .getSingleResult();
+
+            assertEquals(postWithDetailsRecord.post.title, "First post");
+            assertNull(postWithDetailsRecord.details);
+        });
+
         doInJPA(entityManager -> {
             Post post = entityManager.find(Post.class, 1L);
-            PostDetails details = new PostDetails("John Doe");
+            PostDetails details = new PostDetails().setCreatedBy("John Doe");
             details.setPost(post);
             entityManager.persist(details);
+        });
+
+        doInJPA(entityManager -> {
+            PostWithDetailsRecord postWithDetailsRecord = entityManager.createQuery("""
+                select new PostWithDetailsRecord(p, pd)
+                from Post p
+                left join PostDetails pd on pd.id = p.id
+                where p.id = :postId
+                """,
+                PostWithDetailsRecord.class)
+            .setParameter("postId", 1L)
+            .getSingleResult();
+
+            assertEquals(postWithDetailsRecord.post.title, "First post");
+            assertEquals(postWithDetailsRecord.details.createdBy, "John Doe");
         });
 
         doInJPA(entityManager -> {
@@ -55,31 +108,26 @@ public class OneToOneMapsIdTest extends AbstractTest {
     public static class Post {
 
         @Id
-        @GeneratedValue
         private Long id;
 
         private String title;
-
-        public Post() {}
-
-        public Post(String title) {
-            this.title = title;
-        }
 
         public Long getId() {
             return id;
         }
 
-        public void setId(Long id) {
+        public Post setId(Long id) {
             this.id = id;
+            return this;
         }
 
         public String getTitle() {
             return title;
         }
 
-        public void setTitle(String title) {
+        public Post setTitle(String title) {
             this.title = title;
+            return this;
         }
     }
 
@@ -100,35 +148,44 @@ public class OneToOneMapsIdTest extends AbstractTest {
         @MapsId
         private Post post;
 
-        public PostDetails() {}
-
-        public PostDetails(String createdBy) {
-            createdOn = new Date();
-            this.createdBy = createdBy;
-        }
-
         public Long getId() {
             return id;
         }
 
-        public void setId(Long id) {
+        public PostDetails setId(Long id) {
             this.id = id;
+            return this;
         }
 
         public Date getCreatedOn() {
             return createdOn;
         }
 
+        public PostDetails setCreatedOn(Date createdOn) {
+            this.createdOn = createdOn;
+            return this;
+        }
+
         public String getCreatedBy() {
             return createdBy;
+        }
+
+        public PostDetails setCreatedBy(String createdBy) {
+            this.createdBy = createdBy;
+            return this;
         }
 
         public Post getPost() {
             return post;
         }
 
-        public void setPost(Post post) {
+        public PostDetails setPost(Post post) {
             this.post = post;
+            return this;
         }
+    }
+
+    public record PostWithDetailsRecord(Post post, PostDetails details) {
+
     }
 }
