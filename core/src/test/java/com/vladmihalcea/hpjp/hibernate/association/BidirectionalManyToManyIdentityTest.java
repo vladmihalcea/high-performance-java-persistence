c2fe6f740ca1e4f88602b6095a011d57635d89f6
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2026-02-06 11:15:32 +0200

    Add test cases to prove that batching works for some operations even when IDENTITY is used

---
+++ core/src/test/java/com/vladmihalcea/hpjp/hibernate/association/BidirectionalManyToManyIdentityTest.java
@@ -0,0 +1,190 @@
+package com.vladmihalcea.hpjp.hibernate.association;
+
+import com.vladmihalcea.hpjp.util.AbstractTest;
+import com.vladmihalcea.hpjp.util.providers.Database;
+import jakarta.persistence.*;
+import org.hibernate.cfg.AvailableSettings;
+import org.junit.Test;
+
+import java.util.*;
+
+/**
+ * @author Vlad Mihalcea
+ */
+public class BidirectionalManyToManyIdentityTest extends AbstractTest {
+
+    @Override
+    protected Class<?>[] entities() {
+        return new Class<?>[] {
+            Post.class,
+            Tag.class
+        };
+    }
+
+    @Override
+    protected Database database() {
+        return Database.SQLSERVER;
+    }
+
+    @Override
+    protected void additionalProperties(Properties properties) {
+        properties.put(AvailableSettings.STATEMENT_BATCH_SIZE, "100");
+        properties.put(AvailableSettings.ORDER_INSERTS, "100");
+    }
+
+    @Test
+    public void testBatchInserts() {
+        Post post1 = new Post("JPA with Hibernate");
+        Post post2 = new Post("Native Hibernate");
+
+        Tag tag1 = new Tag("Java");
+        Tag tag2 = new Tag("Hibernate");
+
+        doInJPA(entityManager -> {
+            entityManager.persist(post1);
+            entityManager.persist(post2);
+            entityManager.persist(tag1);
+            entityManager.persist(tag2);
+        });
+
+        doInJPA(entityManager -> {
+            Post _post1 = entityManager.find(Post.class, post1.getId());
+            Post _post2 = entityManager.find(Post.class, post2.getId());
+
+            Tag _tag1 = entityManager.find(Tag.class, tag1.getId());
+            Tag _tag2 = entityManager.find(Tag.class, tag2.getId());
+            _post1.addTag(_tag1);
+            _post1.addTag(_tag2);
+
+            _post2.addTag(_tag1);
+        });
+    }
+
+    @Entity(name = "Post")
+    @Table(name = "post")
+    public static class Post {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        private String title;
+
+        public Post() {}
+
+        public Post(String title) {
+            this.title = title;
+        }
+
+        @ManyToMany(cascade = { CascadeType.PERSIST, CascadeType.MERGE})
+        @JoinTable(name = "post_tag",
+            joinColumns = @JoinColumn(name = "post_id"),
+            inverseJoinColumns = @JoinColumn(name = "tag_id")
+        )
+        private List<Tag> tags = new ArrayList<>();
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public List<Tag> getTags() {
+            return tags;
+        }
+
+        public void addTag(Tag tag) {
+            tags.add(tag);
+            tag.getPosts().add(this);
+        }
+
+        public void removeTag(Tag tag) {
+            tags.remove(tag);
+            tag.getPosts().remove(this);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o)
+                return true;
+
+            if (!(o instanceof Post))
+                return false;
+
+            Post post = (Post) o;
+            return Objects.equals(title, post.title);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(title);
+        }
+    }
+
+    @Entity(name = "Tag")
+    @Table(name = "tag")
+    public static class Tag {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        private String name;
+
+        @ManyToMany(mappedBy = "tags")
+        private List<Post> posts = new ArrayList<>();
+
+        public Tag() {}
+
+        public Tag(String name) {
+            this.name = name;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public List<Post> getPosts() {
+            return posts;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o)
+                return true;
+
+            if (!(o instanceof Tag))
+                return false;
+
+            Tag tag = (Tag) o;
+            return Objects.equals(name, tag.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name);
+        }
+    }
+}
---
+++ core/src/test/java/com/vladmihalcea/hpjp/hibernate/association/OneToOneMapsIdIdentityTest.java
@@ -0,0 +1,151 @@
+package com.vladmihalcea.hpjp.hibernate.association;
+
+import com.vladmihalcea.hpjp.util.AbstractTest;
+import com.vladmihalcea.hpjp.util.providers.Database;
+import io.hypersistence.utils.hibernate.type.util.ClassImportIntegrator;
+import jakarta.persistence.*;
+import org.hibernate.cfg.AvailableSettings;
+import org.hibernate.jpa.boot.spi.IntegratorProvider;
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Properties;
+import java.util.stream.LongStream;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Vlad Mihalcea
+ */
+public class OneToOneMapsIdIdentityTest extends AbstractTest {
+
+    @Override
+    protected Class<?>[] entities() {
+        return new Class<?>[] {
+            Post.class,
+            PostDetails.class,
+        };
+    }
+
+    @Override
+    protected Database database() {
+        return Database.SQLSERVER;
+    }
+
+    @Override
+    protected void additionalProperties(Properties properties) {
+        properties.put(AvailableSettings.STATEMENT_BATCH_SIZE, "100");
+        properties.put(AvailableSettings.ORDER_INSERTS, "100");
+    }
+
+    @Test
+    public void testBatchInserts() {
+        doInJPA(entityManager -> {
+            List<Post> posts = LongStream
+                .range(1, 10).boxed()
+                .map(id -> new Post().setTitle(String.format("Post nr. %s", id)))
+                .toList();
+            posts.forEach(entityManager::persist);
+            entityManager.flush();
+            posts.stream()
+                .map(post -> new PostDetails()
+                    .setPost(post)
+                    .setCreatedBy("Vlad Mihalcea"))
+                .forEach(entityManager::persist);
+        });
+    }
+
+    @Entity(name = "Post")
+    @Table(name = "Posts")
+    public static class Post {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name = "PostId")
+        private Long id;
+
+        @Column(name = "Title")
+        private String title;
+
+        public Long getId() {
+            return id;
+        }
+
+        public Post setId(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public Post setTitle(String title) {
+            this.title = title;
+            return this;
+        }
+    }
+
+    @Entity(name = "PostDetails")
+    @Table(name = "PostDetails")
+    public static class PostDetails {
+
+        @Id
+        private Long id;
+
+        @Column(name = "CreatedOn")
+        private LocalDateTime createdOn = LocalDateTime.now();
+
+        @Column(name = "CreatedBy")
+        private String createdBy;
+
+        @OneToOne
+        @MapsId
+        @JoinColumn(name = "PostId")
+        private Post post;
+
+        public Long getId() {
+            return id;
+        }
+
+        public PostDetails setId(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public LocalDateTime getCreatedOn() {
+            return createdOn;
+        }
+
+        public PostDetails setCreatedOn(LocalDateTime createdOn) {
+            this.createdOn = createdOn;
+            return this;
+        }
+
+        public String getCreatedBy() {
+            return createdBy;
+        }
+
+        public PostDetails setCreatedBy(String createdBy) {
+            this.createdBy = createdBy;
+            return this;
+        }
+
+        public Post getPost() {
+            return post;
+        }
+
+        public PostDetails setPost(Post post) {
+            this.post = post;
+            this.id = post.getId();
+            return this;
+        }
+    }
+
+    public record PostWithDetailsRecord(Post post, PostDetails details) {
+
+    }
+}
